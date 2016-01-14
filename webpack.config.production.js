623f4fdfab05fe03d8d3a41680161c05c595a0c7
Pascal Widdershoven <pascal@kabisa.nl>
2016-01-14 07:48:06 +0100

    Fix Webpack publicPath for production builds, fixes #119
    Previous value `/dist/` would cause static assets to be searched for in
    `file:///dist/...` (root of fs), instead of in the `dist/` directory
    relative to `app/index.html`.

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -13,7 +13,7 @@ config.devtool = 'source-map';
 
 config.entry = './app/index';
 
-config.output.publicPath = '/dist/';
+config.output.publicPath = '../dist/';
 
 config.module.loaders.push({
   test: /^((?!\.module).)*\.css$/,
