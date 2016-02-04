fe19ca1ac5ed10ee01ca8f491f37841b1e4b1519
Jhen <developer@jhen.me>
2016-02-05 04:46:55 +0800

    Update redux to v3.2.1 and API usage

--- app/store/configureStore.production.js
+++ app/store/configureStore.production.js
@@ -2,10 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const finalCreateStore = compose(
+const enhancer = compose(
   applyMiddleware(thunk)
-)(createStore);
+);
 
 export default function configureStore(initialState) {
-  return finalCreateStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, enhancer);
 }
