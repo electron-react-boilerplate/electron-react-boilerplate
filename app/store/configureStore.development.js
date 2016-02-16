b179b40bb9aa9427884b08109d3eafe685820f74
C.T. Lin <chentsulin@gmail.com>
2016-02-16 14:36:04 +0800

    use react-router 2.0 final release

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -6,9 +6,9 @@ import { syncHistory } from 'react-router-redux';
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
 
-const reduxRouterMiddleware = syncHistory(hashHistory);
+const router = syncHistory(hashHistory);
 const enhancer = compose(
-  applyMiddleware(thunk, reduxRouterMiddleware),
+  applyMiddleware(thunk, router),
   DevTools.instrument(),
   persistState(
     window.location.href.match(
@@ -20,7 +20,7 @@ const enhancer = compose(
 export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, enhancer);
 
-  reduxRouterMiddleware.listenForReplays(store);
+  router.listenForReplays(store);
 
   if (module.hot) {
     module.hot.accept('../reducers', () =>
--- app/store/configureStore.production.js
+++ app/store/configureStore.production.js
@@ -4,8 +4,8 @@ import { hashHistory } from 'react-router';
 import { syncHistory } from 'react-router-redux';
 import rootReducer from '../reducers';
 
-const reduxRouterMiddleware = syncHistory(hashHistory);
-const enhancer = applyMiddleware(thunk, reduxRouterMiddleware);
+const router = syncHistory(hashHistory);
+const enhancer = applyMiddleware(thunk, router);
 
 export default function configureStore(initialState) {
   return createStore(rootReducer, initialState, enhancer);
