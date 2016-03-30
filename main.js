4d121ffe43c9faf1d6c2079357658d843a18523d
Amine Benseddik <amine.benseddik@pixelfactory.com>
2016-03-30 15:08:37 +0200

    Open application when webcontent is loaded

--- main.js
+++ main.js
@@ -27,9 +27,18 @@ app.on('window-all-closed', () => {
 
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ width: 1024, height: 728 });
+  mainWindow = new BrowserWindow({
+    show: false,
+    width: 1024,
+    height: 728
+  });
 
   mainWindow.loadURL(`file://${__dirname}/app/app.html`);
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.show();
+    mainWindow.focus();
+  });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
