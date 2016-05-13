dfc7aa27bf0a83c5b24c7a7331b4ccbb98f4e45f
C.T. Lin <chentsulin@gmail.com>
2016-05-14 01:53:38 +0800

    Upgraded electron to v1 and remove crashReporter

--- main.development.js
+++ main.development.js
@@ -1,10 +1,9 @@
-import { app, BrowserWindow, Menu, crashReporter, shell } from 'electron';
+import { app, BrowserWindow, Menu, shell } from 'electron';
 
 let menu;
 let template;
 let mainWindow = null;
 
-crashReporter.start();
 
 if (process.env.NODE_ENV === 'development') {
   require('electron-debug')();
