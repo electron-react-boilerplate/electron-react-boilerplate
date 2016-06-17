9456dae6c55b379f608c412a82b06e2de75c7e2c
Amila Welihinda <amilajack@gmail.com>
2016-06-17 13:06:39 -0700

    Added why-did-you-update, updated deps

--- app/containers/DevTools.js
+++ app/containers/DevTools.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import whyDidYouUpdate from 'why-did-you-update';
+
+if (process.env.NODE_ENV !== 'production') {
+  whyDidYouUpdate(React);
+}
 
 export default createDevTools(
   <DockMonitor
