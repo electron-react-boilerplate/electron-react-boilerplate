c27f47b08268d84f0eac0c18045dfaf066bc3fc7
Jhen <developer@jhen.me>
2016-08-04 15:39:21 +0800

    Invoke devToolsExtension.updateStore, let middlewares work

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -29,6 +29,10 @@ const enhancer = compose(
 export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, enhancer);
 
+  if (window.devToolsExtension) {
+    window.devToolsExtension.updateStore(store);
+  }
+
   if (module.hot) {
     module.hot.accept('../reducers', () =>
       store.replaceReducer(require('../reducers')) // eslint-disable-line global-require
