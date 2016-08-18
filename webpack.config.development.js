a138d7748bb2ef4457d9b9d3f9c40fb4c7a8a502
Amila Welihinda <amilajack@users.noreply.github.com>
2016-08-17 21:20:13 -0700

    Update webpack.config.development.js

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -3,13 +3,15 @@ import webpack from 'webpack';
 import merge from 'webpack-merge';
 import baseConfig from './webpack.config.base';
 
+const port = process.env.PORT || 3000;
+
 export default merge(baseConfig, {
   debug: true,
 
   devtool: 'cheap-module-eval-source-map',
 
   entry: [
-    'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
+    `webpack-hot-middleware/client?path=http://localhost:${port}/__webpack_hmr`,
     './app/index'
   ],
 
