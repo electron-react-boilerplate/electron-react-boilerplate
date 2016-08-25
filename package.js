7e4b0d19b49e2f6b932ea5bfe81cc7a5ac99dd89
Vincent Taverna <vinny@getlua.com>
2016-08-24 23:35:51 -0400

    use async/await instead of native promises

--- package.js
+++ package.js
@@ -69,30 +69,31 @@ function build(cfg) {
   });
 }
 
-function startPack() {
+async function startPack() {
   console.log('start pack...');
-  build(electronCfg)
-    .then(() => build(cfg))
-    .then(() => del('release'))
-    .then(paths => {
-      if (shouldBuildAll) {
-        // build for all platforms
-        const archs = ['ia32', 'x64'];
-        const platforms = ['linux', 'win32', 'darwin'];
 
-        platforms.forEach(plat => {
-          archs.forEach(arch => {
-            pack(plat, arch, log(plat, arch));
-          });
+  try {
+    await build(electronCfg);
+    await build(cfg);
+    const paths = await del('release');
+
+    if (shouldBuildAll) {
+      // build for all platforms
+      const archs = ['ia32', 'x64'];
+      const platforms = ['linux', 'win32', 'darwin'];
+
+      platforms.forEach((plat) => {
+        archs.forEach((arch) => {
+          pack(plat, arch, log(plat, arch));
         });
-      } else {
-        // build for current platform only
-        pack(os.platform(), os.arch(), log(os.platform(), os.arch()));
-      }
-    })
-    .catch(err => {
-      console.error(err);
-    });
+      });
+    } else {
+      // build for current platform only
+      pack(os.platform(), os.arch(), log(os.platform(), os.arch()));
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function pack(plat, arch, cb) {
