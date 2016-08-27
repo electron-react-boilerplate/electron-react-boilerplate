7a45a97480fbc91b36c69924062e42da0efacfb7
C. T. Lin <chentsulin@gmail.com>
2016-08-27 09:23:00 +0800

    Fix: replace electron-prebuilt to electron

--- package.js
+++ package.js
@@ -48,11 +48,11 @@ if (version) {
   startPack();
 } else {
   // use the same version as the currently-installed electron-prebuilt
-  exec('npm list electron-prebuilt --dev', (err, stdout) => {
+  exec('npm list electron --dev', (err, stdout) => {
     if (err) {
       DEFAULT_OPTS.version = '1.2.0';
     } else {
-      DEFAULT_OPTS.version = stdout.split('electron-prebuilt@')[1].replace(/\s/g, '');
+      DEFAULT_OPTS.version = stdout.split('electron@')[1].replace(/\s/g, '');
     }
 
     startPack();
