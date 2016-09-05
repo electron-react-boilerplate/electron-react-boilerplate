a8fd73d80e35161183796a1e18eb73fbeb80afbb
Jeff Friesen <momoblackblack@gmail.com>
2016-09-05 11:49:15 -0600

    adding basic async await example

--- app/index.js
+++ app/index.js
@@ -10,6 +10,17 @@ import './app.global.css';
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
 
+function sleep(ms = 0) {
+  return new Promise(r => setTimeout(r, ms));
+}
+async function asyncTest() {
+  console.log('a');
+  await sleep(3000);
+  console.log('b');
+}
+asyncTest();
+
+
 render(
   <Provider store={store}>
     <Router history={history} routes={routes} />
