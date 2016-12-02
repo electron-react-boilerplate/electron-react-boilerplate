fb26aa8ec156c25f143fc52e909e9ac9e1ce77e5
Amila Welihinda <amilajack@gmail.com>
2016-12-02 08:07:57 -0800

    Moved styling from production to renderer

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -17,10 +17,6 @@ export default validate({
     }, {
       test: /\.json$/,
       loader: 'json-loader'
-    },
-    {
-      test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
-      loader: 'url-loader'
     }]
   },
 
