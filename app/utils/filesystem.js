fb472b700081c76d7e94cdde4a852a683c5437a0
Sean McBride <spmcbride1201@gmail.com>
2017-01-03 12:24:06 -0500

    fix: Gracefully handle cancel in open file dialog

--- app/utils/filesystem.js
+++ app/utils/filesystem.js
@@ -2,6 +2,5 @@ import {readFileSync} from 'fs';
 
 export default function loadFile(absPathOfFile) {
   let data = readFileSync(absPathOfFile, 'utf8');
-  console.log(data);
   return data;
 }
--- app/utils/nativeDialogs.js
+++ app/utils/nativeDialogs.js
@@ -1,13 +1,14 @@
 const {dialog} = require('electron').remote;
 import loadFile from './filesystem';
 import { setEditorContent } from '../actions/editor';
-import {store} from '../index';
+import { store } from '../index';
 
 //HACK: Bypassing react-redux to directly dispatch to store.
 export default function openFilePicker() {
   dialog.showOpenDialog({ properties: ['openFile', 'createDirectory', 'showHiddenFiles'] }, function (fileNames) {
-    console.log(fileNames[0]); // an array
-    let fileContents = loadFile(fileNames[0]);
-    store.dispatch(setEditorContent(fileContents));
+    if (fileNames && fileNames.length) {
+      let fileContents = loadFile(fileNames[0]);
+      store.dispatch(setEditorContent(fileContents));
+    }
   })
 }
