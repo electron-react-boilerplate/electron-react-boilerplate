0520cf3f7b0b157a8db3808d5544503b0be07a92
Sean McBride <spmcbride1201@gmail.com>
2017-01-03 23:17:18 -0500

    Remove editor mainframe ipsum

--- app/reducers/editor.js
+++ app/reducers/editor.js
@@ -1,20 +1,8 @@
 import { SET_EDITOR_CONTENT, SET_EDITOR_PATH } from '../constants';
 
 const initialEditorState = {
-  editorContent: '//SAMPJCL JOB 1,CLASS=6,MSGCLASS=0,NOTIFY=&SYSUID\r' +
-  '//*\r' +
-  '//STEP010  EXEC PGM=SORT\r' +
-  '//SORTIN   DD DSN=JCL.SAMPLE.INPUT,DISP=SHR\r' +
-  '//SORTOUT  DD DSN=JCL.SAMPLE.OUTPUT,\r' +
-  '//         DISP=(NEW,CATLG,CATLG),DATACLAS=DSIZE50\r' +
-  '//SYSOUT   DD SYSOUT=*\r' +
-  '//SYSUDUMP DD SYSOUT=C\r' +
-  '//SYSPRINT DD SYSOUT=*\r' +
-  '//SYSIN    DD *\r' +
-  'SORT FIELDS=COPY\r' +
-  "INCLUDE COND=(28,3,CH,EQ,C'XXX')\r" +
-  '/*',
-  editorPath: null
+  editorContent: '',
+  editorPath: ''
 };
 
 export default function (state = initialEditorState, action) {
