2a523d9c7ce6e29ea47eddcb64baeaea1d5cbf38
Sean McBride <spmcbride1201@gmail.com>
2017-01-05 00:00:42 -0500

    feat: Add mainframe icon

--- app/utils/renderIcon.js
+++ app/utils/renderIcon.js
@@ -79,6 +79,51 @@ export default function renderIcon(name, theme) {
           </g>
         </svg>
       )
+    case 'mainframe':
+      return (
+        <svg viewBox="0 0 280.14 384.55" width="40px" height="36px">
+          <title>mainframe</title>
+          <g id="Layer_1-2" data-name="Layer 1">
+            <rect x="2" y="39.5" width="276.14" height="343.05" fill='#999' stroke='#000' strokeWidth='4px' />
+            <line x1="2" y1="110" x2="278.14" y2="110" fill='none' stroke='#231f20' strokeWidth='4px' />
+            <line x1="147.27" y1="39.5" x2="149.1" y2="110" fill='none' stroke='#231f20' strokeWidth='4px' />
+            <line x1="226.64" y1="39.5" x2="226.64" y2="110" fill='none' stroke='#231f20' strokeWidth='4px' />
+            <line x1="2" y1="300.86" x2="278.14" y2="300.86" fill='none' stroke='#231f20' strokeWidth='4px' />
+            <line x1="186.5" y1="382.55" x2="186.5" y2="301.86" fill='none' stroke='#231f20' strokeWidth='4px' />
+            <line x1="53.69" y1="110" x2="53.14" y2="300.86" fill='none' stroke='#231f20' strokeWidth='4px' />
+            <rect x="39.5" y="2" width="203.41" height="26.14" fill='#cccccb' stroke='#231f20' strokeWidth='4px' />
+            <line x1="92.91" y1="39.5" x2="92.91" y2="28.14" fill='none' stroke='#231f20' strokeWidth='4px' />
+            <line x1="183.91" y1="40.5" x2="183.91" y2="29.14" fill='none' stroke='#231f20' strokeWidth='4px' />
+            <rect x="39.5" y="57.68" width="69.32" height="28.41" fill='#fff' stroke='#231f20' strokeWidth='4px' />
+            <line x1="74.16" y1="86.09" x2="92.91" y2="64.5" fill='none' stroke='#231f20' strokeWidth='4px' />
+            <rect x="199.73" y="329" width="19.89" height="19.89" fill='#39b54a' stroke='#231f20' strokeWidth='4px' />
+            <rect x="242.91" y="329" width="19.89" height="19.89" fill='#ed1c24' stroke='#231f20' strokeWidth='4px'/>
+            <rect x="67.91" y="129.27" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px' />
+            <rect x="67.91" y="152" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px'/>
+            <rect x="173.75" y="129.27" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px'/>
+            <rect x="173.75" y="152" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px' />
+            <rect x="173.75" y="175.8" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px'/>
+            <rect x="173.75" y="199.59" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px'/>
+            <rect x="68.17" y="175.8" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px' />
+            <rect x="68.17" y="199.59" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px'/>
+            <rect x="67.91" y="222.07" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px'/>
+            <rect x="173.75" y="222.07" width="92.05" height="11.36" fill='#231f20' stroke='#231f20' strokeWidth='4px' />
+            <rect x="67.91" y="245.33" width="197.89" height="5.68" fill='#231f20' stroke='#231f20' strokeWidth='4px'/>
+            <rect x="67.91" y="261.6" width="197.89" height="5.68" fill='#231f20' stroke='#231f20' strokeWidth='4px'/>
+            <rect x="67.91" y="277.86" width="197.89" height="5.68" fill='#231f20' stroke='#231f20' strokeWidth='4px' />
+            <circle cx="253.14" cy="75.86" r="10.23" fill='#666766' stroke='#231f20' strokeWidth='4px'/>
+            <circle cx="204.7" cy="62.65" r="4.97" stroke='#231f20' strokeWidth='4px' />
+            <circle cx="204.7" cy="85.65" r="4.97" stroke='#231f20' strokeWidth='4px' />
+            <circle cx="184.7" cy="85.65" r="4.97" stroke='#231f20' strokeWidth='4px' />
+            <circle cx="164.42" cy="85.65" r="4.97" stroke='#231f20' strokeWidth='4px' />
+            <rect x="130.41" y="314.5" width="43.32" height="51.59" fill='#999' stroke='#fff' strokeWidth='4px' />
+            <rect x="83.53" y="327" width="31.03" height="39.09" fill='#999' stroke='#fff' strokeWidth='4px'/>
+            <circle cx="99.05" cy="337.95" r="4.97" stroke='#231f20' strokeWidth='4px'/>
+            <circle cx="99.05" cy="355.43" r="4.97" stroke='#231f20' strokeWidth='4px' />
+            <rect x="14.5" y="340.95" width="53.41" height="10.65" fill='#231f20' stroke='#231f20' strokeWidth='4px'/>
+          </g>
+        </svg>
+      )
   }
 }
 
