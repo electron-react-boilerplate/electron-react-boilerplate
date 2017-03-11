a257fa117aba67bec796e5d3f083e26f088cb8c1
Amila Welihinda <amilajack@users.noreply.github.com>
2017-03-11 13:53:55 -0800

    Added flow types to Root.js

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,17 +1,18 @@
-import React, { PropTypes } from 'react';
+// @flow
+import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import routes from '../routes';
 
-export default function Root({ store, history }) {
+type RootType = {
+  store: {},
+  history: {}
+};
+
+export default function Root({ store, history }: RootType) {
   return (
     <Provider store={store}>
       <Router history={history} routes={routes} />
     </Provider>
   );
 }
-
-Root.propTypes = {
-  store: PropTypes.shape({}).isRequired,
-  history: PropTypes.shape({}).isRequired,
-};
