896b98423f3f2c6d1374d3b0454de0f35b91339b
Amila Welihinda <amilajack@users.noreply.github.com>
2017-03-29 19:23:43 -0700

    Fixed trailing space

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -118,7 +118,7 @@ export default merge(baseConfig, {
      *
      * NODE_ENV should be production so that modules do not perform certain
      * development checks
-     * 
+     *
      * By default, use 'development' as NODE_ENV. This can be overriden with
      * 'staging', for example, by changing the ENV variables in the npm scripts
      */
