b64264e621713bcedf54d9cfa628d42c8e61aa70
Amila Welihinda <amilajack@users.noreply.github.com>
2017-03-30 18:08:43 -0700

    More flexible NODE_ENV with DefinePlugin (#866)
    * More flexible NODE_ENV with DefinePlugin
    
    * Fixed trailing space

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -118,9 +118,12 @@ export default merge(baseConfig, {
      *
      * NODE_ENV should be production so that modules do not perform certain
      * development checks
+     *
+     * By default, use 'development' as NODE_ENV. This can be overriden with
+     * 'staging', for example, by changing the ENV variables in the npm scripts
      */
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
     // turn debug mode on.
     new webpack.LoaderOptionsPlugin({
