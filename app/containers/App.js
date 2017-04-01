ea29d9cd46e1e93269fe7910ce9a4dbb30281cc7
Amila Welihinda <amilajack@gmail.com>
2017-03-31 17:07:34 -0700

    Updated all deps

--- app/containers/App.js
+++ app/containers/App.js
@@ -10,7 +10,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>some foo</h1>
         {this.props.children}
       </div>
     );
--- app/routes.js
+++ app/routes.js
@@ -1,37 +1,13 @@
 // @flow
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
-import Bundle from './containers/Bundle';
+import HomePage from './containers/HomePage';
+import CounterPage from './containers/CounterPage';
 
-/* eslint flowtype-errors/show-errors: 0 */
-
-const HomePage = () => (
-  // $FlowIssue: https://github.com/facebook/flow/pull/3544
-  <Bundle load={() => import('./containers/HomePage')}>
-    {HomePageComponent => (HomePageComponent
-      ? <HomePageComponent />
-      : <div> Loading HomePage... </div>)}
-  </Bundle>
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="/counter" component={CounterPage} />
+  </Route>
 );
-
-const CounterPage = () => (
-  // $FlowIssue: https://github.com/facebook/flow/pull/3544
-  <Bundle load={() => import('./containers/CounterPage')}>
-    {(CounterPageComponent) => (CounterPageComponent
-      ? <CounterPageComponent />
-      : <div> Loading CounterPage... </div>)
-    }
-  </Bundle>
-);
-
-const Routes = () => (
-  <App>
-    <Switch>
-      <Route path="/counter" component={CounterPage} />
-      <Route path="/" component={HomePage} />
-    </Switch>
-  </App>
-);
-
-export default Routes;
