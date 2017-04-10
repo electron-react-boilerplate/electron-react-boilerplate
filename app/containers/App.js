c6a9b3797f670ddf276b97d1e35acf50e37be934
thisconnect <hi@this-connect.me>
2017-04-10 09:32:29 +0200

    flow again use any so we only get a warning

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,10 +1,9 @@
 // @flow
 import React, { Component } from 'react';
-import type { Children } from 'react';
 
 export default class App extends Component {
   props: {
-    children: Children
+    children: any
   };
 
   render() {
