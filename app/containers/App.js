a503def705bd6fcb1909cbe7ad1213a3ad9bf0d6
thisconnect <hi@this-connect.me>
2017-04-10 09:17:42 +0200

    flow again use Children, passes npm run lint locally

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,10 +1,10 @@
 // @flow
 import React, { Component } from 'react';
-import type { Element } from 'react';
+import type { Children } from 'react';
 
 export default class App extends Component {
   props: {
-    children: Element<*>
+    children: Children
   };
 
   render() {
