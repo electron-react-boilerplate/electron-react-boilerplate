e8f58b3d12d3be8a41b5e3e3b36bf02f55970da5
Amila Welihinda <amilajack@gmail.com>
2017-04-22 10:19:32 -0700

    Revert "Added HtmlWebpackPlugin (#916)"
    This reverts commit f36e9d26791d2c2389187cbdb9647f7335578516.

--- webpack.config.renderer.dev.js
+++ webpack.config.renderer.dev.js
@@ -12,10 +12,8 @@ import fs from 'fs';
 import webpack from 'webpack';
 import chalk from 'chalk';
 import merge from 'webpack-merge';
-import express from 'express';
 import { spawn, execSync } from 'child_process';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import baseConfig from './webpack.config.base';
 
 const port = process.env.PORT || 1212;
@@ -217,15 +215,6 @@ export default merge.smart(baseConfig, {
 
     new ExtractTextPlugin({
       filename: '[name].css'
-    }),
-
-    /**
-     * Dynamically generate index.html page
-     */
-    new HtmlWebpackPlugin({
-      filename: 'app.html',
-      template: 'app/app.html',
-      dll: `${publicPath}/dll/vendor.dll.js`
     })
   ],
 
@@ -249,9 +238,7 @@ export default merge.smart(baseConfig, {
       rewrites: [{ from: /./, to: '/dist/app.html' }],
       disableDotRule: false,
     },
-    setup(app) {
-      app.use('/dist/dll/', express.static(dll));
-
+    setup() {
       if (process.env.START_HOT) {
         spawn(
           'npm',
