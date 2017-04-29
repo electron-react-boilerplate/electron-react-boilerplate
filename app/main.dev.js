f817a8424153b9b6c497620691fd36d18941ae5d
Amila Welihinda <amilajack@gmail.com>
2017-04-29 16:53:05 -0700

    Updated CHANGELOG

--- app/main.dev.js
+++ app/main.dev.js
@@ -24,7 +24,6 @@ if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true')
   require('electron-debug')();
   const path = require('path');
   const p = path.join(__dirname, '..', 'app', 'node_modules');
-  // $FlowIssue: No type definitions for 'module' yet
   require('module').globalPaths.push(p);
 }
 
