8d1509c884c9357cc1b7b6cab666a80e0d60ab7c
Amila Welihinda <amilajack@gmail.com>
2017-06-30 08:40:58 -0700

    Renamed webpack output filenames, fixed watchOptions of dev-server

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -24,7 +24,7 @@ export default {
 
   output: {
     path: path.join(__dirname, 'app'),
-    filename: 'bundle.js',
+    filename: 'renderer.dev.js',
     // https://github.com/webpack/webpack/issues/1114
     libraryTarget: 'commonjs2'
   },
