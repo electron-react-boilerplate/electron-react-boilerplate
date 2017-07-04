d3abb85704ea94c0e3ce20a36a8bfd819f6c6f18
Amila Welihinda <amilajack@gmail.com>
2017-07-04 00:06:31 -0700

    Fixed linter errors

--- internals/scripts/CheckBuiltsExist.js
+++ internals/scripts/CheckBuiltsExist.js
@@ -10,13 +10,13 @@ function CheckBuildsExist() {
 
   if (!fs.existsSync(mainPath)) {
     throw new Error(chalk.whiteBright.bgRed.bold(
-      `The main process is not built yet. Build it by running "npm run build-main"`
+      'The main process is not built yet. Build it by running "npm run build-main"'
     ));
   }
 
   if (!fs.existsSync(rendererPath)) {
     throw new Error(chalk.whiteBright.bgRed.bold(
-      `The renderer process is not built yet. Build it by running "npm run build-renderer"`
+      'The renderer process is not built yet. Build it by running "npm run build-renderer"'
     ));
   }
 }
