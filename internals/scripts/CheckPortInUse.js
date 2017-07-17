d9c1e0d071f1d84593d8abcd14ca57ee98f5f9ce
Amila Welihinda <amilajack@gmail.com>
2017-07-17 13:34:05 -0700

    Check if PORT in use before starting dev env

---
+++ internals/scripts/CheckPortInUse.js
@@ -0,0 +1,15 @@
+// @flow
+import chalk from 'chalk';
+import detectPort from 'detect-port';
+
+(function CheckPortInUse() {
+  const port = process.env.PORT || 1212;
+
+  detectPort(port, (err, availablePort) => {
+    if (port !== availablePort) {
+      throw new Error(chalk.whiteBright.bgRed.bold(
+        `Port "${port}" on "localhost" is already in use. Please use another port`
+      ));
+    }
+  });
+})()
