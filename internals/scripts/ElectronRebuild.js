f66e1f22afefff10507531bd3796f89e87788fa1
Amila Welihinda <amilajack@gmail.com>
2018-01-08 11:44:08 -0800

    Fixed cwd for postinstall script

--- internals/scripts/ElectronRebuild.js
+++ internals/scripts/ElectronRebuild.js
@@ -1,11 +1,14 @@
 // @flow
+import path from 'path';
 import { execSync } from 'child_process';
 
 const electronRebuildCmd =
-  '../../node_modules/.bin/electron-rebuild --parallel --force --types prod,dev,optional --module-dir ../../app';
+  '../node_modules/.bin/electron-rebuild --parallel --force --types prod,dev,optional --module-dir .';
 
-if (process.platform === 'win32') {
-  execSync(electronRebuildCmd.replace(/\//g, '\\'));
-} else {
-  execSync(electronRebuildCmd);
-}
+const cmd = process.platform === 'win32'
+  ? electronRebuildCmd.replace(/\//g, '\\')
+  : electronRebuildCmd;
+
+execSync(cmd, {
+  cwd: path.join(__dirname, '..', '..', 'app')
+});
