dc3fc46ed861c76c7f937ac097fde946f8a4315c
Amila Welihinda <amilajack@gmail.com>
2018-01-08 12:13:20 -0800

    Attempt fix for cross-platform test path

--- test/runTests.js
+++ test/runTests.js
@@ -1,7 +1,7 @@
 const spawn = require('cross-spawn');
 const path = require('path');
 
-const s = `\\${path.sep}`;
+const s = '/';
 const pattern = process.argv[2] === 'e2e'
   ? `test${s}e2e${s}.+\\.spec\\.js`
   : `test${s}(?!e2e${s})[^${s}]+${s}.+\\.spec\\.js$`;
