a2a5a871f0f9760c97be5169ea7997189ff14a73
Amila Welihinda <amilajack@gmail.com>
2018-01-09 00:45:06 -0800

    Fix electron-rebuild script

--- internals/scripts/ElectronRebuild.js
+++ internals/scripts/ElectronRebuild.js
@@ -1,14 +1,17 @@
 // @flow
 import path from 'path';
 import { execSync } from 'child_process';
+import dependencies from '../../app/package.json';
 
-const electronRebuildCmd =
+if (dependencies.length > 0) {
+  const electronRebuildCmd =
   '../node_modules/.bin/electron-rebuild --parallel --force --types prod,dev,optional --module-dir .';
 
-const cmd = process.platform === 'win32'
-  ? electronRebuildCmd.replace(/\//g, '\\')
-  : electronRebuildCmd;
+  const cmd = process.platform === 'win32'
+    ? electronRebuildCmd.replace(/\//g, '\\')
+    : electronRebuildCmd;
 
-execSync(cmd, {
-  cwd: path.join(__dirname, '..', '..', 'app')
-});
+  execSync(cmd, {
+    cwd: path.join(__dirname, '..', '..', 'app')
+  });
+}
