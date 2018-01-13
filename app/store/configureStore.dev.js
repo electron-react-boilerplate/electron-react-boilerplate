6b44c9f960ccc86141ed5d539fdf4cf20e93aaca
Vytenis <projektas@gmail.com>
2018-01-13 23:21:51 +0200

    Update configureStore.dev.js (#1371)
    Skip redux logs in console during the tests

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -22,7 +22,11 @@ const configureStore = (initialState?: counterStateType) => {
     level: 'info',
     collapsed: true
   });
-  middleware.push(logger);
+
+  // Skip redux logs in console during the tests
+  if (process.env.NODE_ENV !== 'test') {
+    middleware.push(logger);
+  }
 
   // Router Middleware
   const router = routerMiddleware(history);
