2d4a75bceb1accd118b8c1f866c5648b69ad8312
Brendan Boyle <brendancboyle@me.com>
2018-02-04 13:28:47 -0500

    Added SendKeys test

--- app/components/Login.js
+++ app/components/Login.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import styles from './Login.css';
 import TokenForm from '../containers/TokenForm';
 import ConnectButton from '../components/ConnectButton';
+import Logo from '../images/icon.png';
 
 type Props = {
   token: string
@@ -15,7 +16,7 @@ export default class Home extends Component<Props> {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <img src="images/icon.png" alt="Logo" />
+          <img src={Logo} alt="Logo" />
           <h2>Universal Presenter Remote</h2>
           <TokenForm />
           <ConnectButton token={this.props.token} />
