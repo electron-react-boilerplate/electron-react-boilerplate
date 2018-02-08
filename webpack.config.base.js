892402d6be9d46e5471efd9f48bdeb1bd3271e37
Amila Welihinda <amilajack@gmail.com>
2018-02-07 22:37:30 -0800

    More consistent node path

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -43,7 +43,16 @@ export default {
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'production'
     }),
-
     new webpack.NamedModulesPlugin(),
   ],
+
+  /**
+   * Disables webpack processing of __dirname and __filename.
+   * If you run the bundle in node.js it falls back to these values of node.js.
+   * https://github.com/webpack/webpack/issues/2010
+   */
+  node: {
+    __dirname: false,
+    __filename: false
+  },
 };
--- webpack.config.main.prod.js
+++ webpack.config.main.prod.js
@@ -48,14 +48,4 @@ export default merge.smart(baseConfig, {
       DEBUG_PROD: 'false'
     })
   ],
-
-  /**
-   * Disables webpack processing of __dirname and __filename.
-   * If you run the bundle in node.js it falls back to these values of node.js.
-   * https://github.com/webpack/webpack/issues/2010
-   */
-  node: {
-    __dirname: false,
-    __filename: false
-  },
 });
