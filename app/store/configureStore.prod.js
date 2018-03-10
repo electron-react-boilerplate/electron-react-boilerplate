30aca607f25c774e5766fd8aeca48ead29e17a63
Casey Britt <yourfavorite@users.noreply.github.com>
2018-03-10 12:59:24 -0800

    createBrowserHistory to createHashHistory for prod (#1184)

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,12 +1,12 @@
 // @flow
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createBrowserHistory } from 'history';
+import { createHashHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 import type { counterStateType } from '../reducers/counter';
 
-const history = createBrowserHistory();
+const history = createHashHistory();
 const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk, router);
 
