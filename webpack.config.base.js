d24228004dfb6bf7004be447ab01555c1df570d0
Amila Welihinda <amilajack@gmail.com>
2018-03-11 13:06:48 -0700

    Filter deps without entrypoint from dll

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -4,10 +4,34 @@
 
 import path from 'path';
 import webpack from 'webpack';
+import fs from 'fs';
 import { dependencies as externals } from './app/package.json';
+import { dependencies as possibleExternals } from './package.json';
+
+// Find all the dependencies without a `main` property and add them as webpack externals
+function filterDepWithoutEntryPoints(dep: string): boolean {
+  // Return true if we want to add a dependency to externals
+  try {
+    // If the root of the dependency has an index.js, return true
+    if (fs.existsSync(path.join(__dirname, `node_modules/${dep}/index.js`))) {
+      return false;
+    }
+    const pgkString =
+      fs.readFileSync(path.join(__dirname, `node_modules/${dep}/package.json`)).toString();
+    const pkg = JSON.parse(pgkString);
+    const fields = ['main', 'module', 'jsnext:main', 'browser'];
+    return !fields.some(field => field in pkg);
+  } catch(e) {
+    console.log(e);
+    return true;
+  }
+}
 
 export default {
-  externals: Object.keys(externals || {}),
+  externals: [
+    ...Object.keys(externals || {}),
+    ...Object.keys(possibleExternals || {}).filter(filterDepWithoutEntryPoints)
+  ],
 
   module: {
     rules: [
