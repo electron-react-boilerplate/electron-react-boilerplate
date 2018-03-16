0821318bb94f19336dbca03bfa6bc856013e55a1
gusarovecejlo <vecejlo@gmail.com>
2018-03-16 14:00:25 +0300

    enable project

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,11 @@
 // @flow
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import styles from './Home.css';
 
 type Props = {};
+
+const REACT_VERSION = React.version
 
 export default class Home extends Component<Props> {
   props: Props;
@@ -12,7 +14,11 @@ export default class Home extends Component<Props> {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
+          <h3>Добро пожаловать, здесь мы будем разрабатывать какое то приложение</h3>
+          <p>Ниже, можно изучить малую презентацию того,</p>
+          <p> как будет работать React внутри Electron</p>
+          <p>В коробке мы имеем уже React {`${REACT_VERSION}`}</p>
+          <p>Redux, Electron, TypeScript, eslintr и еще кучу другого (хлама?)</p>
           <Link to="/counter">to Counter</Link>
         </div>
       </div>
