83df6158e7ac30c2b34cc1cfc946c94865d3cea0
Eric Pyle <eric_pyle@sil.org>
2018-04-02 15:57:28 -0500

    first commit

--- app/components/Home.js
+++ app/components/Home.js
@@ -12,7 +12,7 @@ export default class Home extends Component<Props> {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
+          <h2>dbl.local Electron</h2>
           <Link to="/counter">to Counter</Link>
         </div>
       </div>
