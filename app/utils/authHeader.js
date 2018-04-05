87be2cab1c19f35de53411018910ddf113964b9a
Eric Pyle <eric_pyle@sil.org>
2018-04-05 17:04:07 -0500

    begin defining basic authentication services

---
+++ app/utils/authHeader.js
@@ -0,0 +1,9 @@
+export default function authHeader() {
+  // return authorization header with jwt token
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user && user.token) {
+    return { Authorization: `Bearer ${user.token}` };
+  }
+  return {};
+}
---
+++ app/utils/user.service.js
@@ -0,0 +1,98 @@
+import { authHeader } from './authHeader';
+
+export const userService = {
+    login,
+    logout,
+    register,
+    getAll,
+    getById,
+    update,
+    delete: _delete
+};
+
+function login(username, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    };
+
+    return fetch('/users/authenticate', requestOptions)
+        .then(response => {
+            if (!response.ok) { 
+                return Promise.reject(response.statusText);
+            }
+
+            return response.json();
+        })
+        .then(user => {
+            // login successful if there's a jwt token in the response
+            if (user && user.token) {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+
+            return user;
+        });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch('/users', requestOptions).then(handleResponse);
+}
+
+function getById(id) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch('/users/' + _id, requestOptions).then(handleResponse);
+}
+
+function register(user) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch('/users/register', requestOptions).then(handleResponse);
+}
+
+function update(user) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch('/users/' + user.id, requestOptions).then(handleResponse);;
+}
+
+// prefixed function name with underscore because delete is a reserved word in javascript
+function _delete(id) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader()
+    };
+
+    return fetch('/users/' + id, requestOptions).then(handleResponse);;
+}
+
+function handleResponse(response) {
+    if (!response.ok) { 
+        return Promise.reject(response.statusText);
+    }
+
+    return response.json();
+}
