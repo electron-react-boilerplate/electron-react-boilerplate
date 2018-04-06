29041f6f7ac379c62284a0200aeb6591ef9b34fb
Eric Pyle <eric_pyle@sil.org>
2018-04-06 15:34:07 -0500

    disable store.js in index

--- app/helpers/store.js
+++ app/helpers/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import rootReducer from '../_reducers';
+import rootReducer from '../reducers';
 
 const loggerMiddleware = createLogger();
 
