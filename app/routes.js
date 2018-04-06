55821eeb9dd85552f87431235748ac4e651ffdf6
Eric Pyle <eric_pyle@sil.org>
2018-04-06 15:11:53 -0500

    remove route to /counter

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
 import LoginPage from './containers/LoginPage';
 import { PrivateRoute } from './components/PrivateRoute';
 
@@ -11,7 +10,6 @@ export default () => (
   <App>
     <Switch>
       <Route path="/login" component={LoginPage} />
-      <Route path="/counter" component={CounterPage} />
       <PrivateRoute exact path="/" component={HomePage} />
     </Switch>
   </App>
