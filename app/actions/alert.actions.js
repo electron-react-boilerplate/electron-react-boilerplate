5dc956cda6ddb2aab924563be531de2efe734921
Eric Pyle <eric_pyle@sil.org>
2018-04-11 13:52:28 -0500

    restructure login promise change to handle error details. code cleanup

--- app/actions/alert.actions.js
+++ app/actions/alert.actions.js
@@ -1,19 +1,19 @@
 import { alertConstants } from '../constants';
 
 export const alertActions = {
-    success,
-    error,
-    clear
+  success,
+  error,
+  clear
 };
 
 function success(message) {
-    return { type: alertConstants.SUCCESS, message };
+  return { type: alertConstants.SUCCESS, message };
 }
 
 function error(message) {
-    return { type: alertConstants.ERROR, message };
+  return { type: alertConstants.ERROR, message };
 }
 
 function clear() {
-    return { type: alertConstants.CLEAR };
+  return { type: alertConstants.CLEAR };
 }
--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,98 +1,95 @@
 import { authHeader } from '../helpers';
 
+
 export const userService = {
-    login,
-    logout,
-    register,
-    getAll,
-    getById,
-    update,
-    delete: _delete
+  login,
+  logout,
+  register,
+  getAll,
+  getById,
+  update,
+  delete: _delete
 };
 
 function login(username, password) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: `email=${username}&password=${password}`
-    };
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `email=${username}&password=${password}`
+  };
 
-    return fetch('https://api.thedigitalbiblelibrary.org/auth/login', requestOptions)
-        .then(response => {
-            if (!response.ok) { 
-                return Promise.reject(response.statusText);
-            }
-
-            return response.json();
-        })
-        .then(user => {
-            // login successful if there's a jwt token in the response
-            if (user && user.auth_token) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
-            }
-
-            return user;
-        });
+  return fetch('https://api.thedigitalbiblelibrary.org/auth/login', requestOptions)
+    .then(response => response.json())
+    .then(json => {
+      // login successful if there's a jwt token in the response
+      if (json && json.auth_token) {
+        // store user details and jwt token in local storage
+        // to keep user logged in between page refreshes
+        localStorage.setItem('user', JSON.stringify(json));
+        return json;
+      }
+      const errorMsg = `Error (${json.error_code} ${json.status_code}): ${json.message}`;
+      return Promise.reject(errorMsg);
+    });
 }
 
 function logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('user');
+  // remove user from local storage to log user out
+  localStorage.removeItem('user');
 }
 
 function getAll() {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader()
+  };
 
-    return fetch('/users', requestOptions).then(handleResponse);
+  return fetch('/users', requestOptions).then(handleResponse);
 }
 
 function getById(id) {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader()
+  };
 
-    return fetch('/users/' + _id, requestOptions).then(handleResponse);
+  return fetch(`/users/${id}`, requestOptions).then(handleResponse);
 }
 
 function register(user) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(user)
-    };
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user)
+  };
 
-    return fetch('/users/register', requestOptions).then(handleResponse);
+  return fetch('/users/register', requestOptions).then(handleResponse);
 }
 
 function update(user) {
-    const requestOptions = {
-        method: 'PUT',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify(user)
-    };
+  const requestOptions = {
+    method: 'PUT',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' },
+    body: JSON.stringify(user)
+  };
 
-    return fetch('/users/' + user.id, requestOptions).then(handleResponse);;
+  return fetch(`/users/${user.id}`, requestOptions).then(handleResponse);
 }
 
 // prefixed function name with underscore because delete is a reserved word in javascript
 function _delete(id) {
-    const requestOptions = {
-        method: 'DELETE',
-        headers: authHeader()
-    };
+  const requestOptions = {
+    method: 'DELETE',
+    headers: authHeader()
+  };
 
-    return fetch('/users/' + id, requestOptions).then(handleResponse);;
+  return fetch(`/users/${id}`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
-    if (!response.ok) { 
-        return Promise.reject(response.statusText);
-    }
+  if (!response.ok) {
+    return Promise.reject(response.statusText);
+  }
 
-    return response.json();
+  return response.json();
 }
