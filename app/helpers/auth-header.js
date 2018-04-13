aae8202cd15dd18d04b9dc1e50a41389dc5d6aec
Eric Pyle <eric_pyle@sil.org>
2018-04-13 16:16:22 -0500

    add authorization header to logout

--- app/helpers/auth-header.js
+++ app/helpers/auth-header.js
@@ -1,10 +1,11 @@
 export function authHeader() {
-    // return authorization header with jwt token
-    let user = JSON.parse(localStorage.getItem('user'));
+  // return authorization header with jwt token
+  const user = JSON.parse(localStorage.getItem('user'));
 
-    if (user && user.auth_token) {
-        return { 'Authorization': 'Bearer ' + user.auth_token };
-    } else {
-        return {};
-    }
+  if (user && user.auth_token) {
+    return { Authorization: `Bearer ${user.auth_token}` };
+  }
+  return {};
 }
+
+export default authHeader;
