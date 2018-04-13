91a1695f7c14bc400ae6b06d7c6b920ce48eaa9b
Eric Pyle <eric_pyle@sil.org>
2018-04-13 10:38:42 -0500

    begin eslint cleanup for PrivateRoute

--- app/components/PrivateRoute.js
+++ app/components/PrivateRoute.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
+type Props = {
+    component: React.Node
+};
+
 /*
  * From https://github.com/cornflourblue/react-redux-registration-login-example/blob/master/src/_components/PrivateRoute.jsx
  */
 export const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
+  <Route
+    {...rest}
+    render={props => (
         localStorage.getItem('user')
             ? <Component {...props} />
             : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-    )} />
+    )}
+  />
 );
+
+export default PrivateRoute;
