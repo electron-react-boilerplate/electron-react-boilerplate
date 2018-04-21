01e327aa9675f4b32d41ab77f44d10db18641a8f
talmukund <bal.mukund015435@gmail.com>
2018-04-21 15:41:32 +0530

    Added auto-update-app feature for electron react boilerplate

--- app/main.dev.js
+++ app/main.dev.js
@@ -10,8 +10,37 @@
  *
  * @flow
  */
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, dialog } from 'electron';
+import { autoUpdater } from 'electron-updater';
+import path from 'path';
+import fs from 'fs';
 import MenuBuilder from './menu';
+
+const log = require('electron-log');
+
+
+function logger() {
+  // Same as for console transport
+  log.transports.file.level = 'warn';
+  log.transports.file.format = '{h}:{i}:{s}:{ms} {text}';
+
+  // Set approximate maximum log size in bytes. When it exceeds,
+  // the archived log will be saved as the log.old.log file
+  log.transports.file.maxSize = 5 * 1024 * 1024;
+
+  // Write to this file, must be set before first logging
+  log.transports.file.file = path.join(__dirname, '/log.txt');
+
+  // fs.createWriteStream options, must be set before first logging
+  // you can find more information at
+  // https://nodejs.org/api/fs.html#fs_fs_createwritestream_path_options
+  log.transports.file.streamConfig = { flags: 'w' };
+
+  // set existed file stream
+  log.transports.file.stream = fs.createWriteStream('log.txt');
+}
+
+logger();
 
 let mainWindow = null;
 
@@ -22,7 +51,6 @@ if (process.env.NODE_ENV === 'production') {
 
 if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
   require('electron-debug')();
-  const path = require('path');
   const p = path.join(__dirname, '..', 'app', 'node_modules');
   require('module').globalPaths.push(p);
 }
@@ -40,6 +68,46 @@ const installExtensions = async () => {
     .catch(console.log);
 };
 
+const connectAutoUpdater = () => {
+  autoUpdater.autoDownload = false;
+  autoUpdater.logger = log;
+
+  autoUpdater.on("error", e => {
+    log.error(`update error ${e.message}`);
+  });
+  autoUpdater.on("update-available", () => {
+    log.info("Update is available so prompting user to update");
+    autoUpdater.downloadUpdate();
+  });
+  autoUpdater.on("checking-for-update", () => {
+    log.info("checking-for-update");
+  });
+  autoUpdater.on("update-not-available", () => {
+    log.info("update-not-available");
+    // showMainWindow();
+  });
+  autoUpdater.on("download-progress", progressObj => {
+    let msg = `Download speed: ${progressObj.bytesPerSecond}`;
+    msg = `${msg} - Downloaded ${progressObj.percent}%`;
+    msg = `${msg} (${progressObj.transferred}/${progressObj.total})`;
+    log.info(msg);
+  });
+  autoUpdater.on("update-downloaded", () => {
+    log.info("update-downloaded");
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }
+
+    dialog.showMessageBox(dialogOpts, (response) => {
+      if (response === 0) autoUpdater.quitAndInstall()
+    });
+  });
+};
+
+connectAutoUpdater();
 
 /**
  * Add event listeners...
@@ -67,6 +135,7 @@ app.on('ready', async () => {
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
 
+  autoUpdater.checkForUpdates();
   // @TODO: Use 'ready-to-show' event
   //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
   mainWindow.webContents.on('did-finish-load', () => {
