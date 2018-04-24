f58f9cfee0c5d53d5804fffe2675c632a101c034
Eric <eric.d.pyle@gmail.com>
2018-04-24 17:18:34 -0500

    fix "Browser history not working in electron app"
    see https://github.com/chentsulin/electron-react-boilerplate/issues/1178

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -6,7 +6,9 @@ import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 import type { counterStateType } from '../reducers/counter';
 
-const history = createBrowserHistory();
+const history = createBrowserHistory({
+  basename: window.location.pathname
+});
 const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk, router);
 
