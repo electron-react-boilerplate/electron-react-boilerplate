8230789e0938521e11f510ab9dd553fb3690463c
Eric Pyle <eric_pyle@sil.org>
2018-04-25 11:47:47 -0500

    redo default port

--- app/constants/dblDotLocal.constants.js
+++ app/constants/dblDotLocal.constants.js
@@ -1,5 +1,5 @@
 export const dblDotLocalConstants = {
-  HTTP_DBL_DOT_LOCAL_BASE_URL: 'http://127.0.0.1:44115'
+  HTTP_DBL_DOT_LOCAL_BASE_URL: 'http://127.0.0.1:44151'
 };
 
 export default dblDotLocalConstants;
