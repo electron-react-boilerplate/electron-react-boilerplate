3b25eae6bd0def356b3c95181fbb8ddb86ccae52
Andrew Allen <allenan@helium.com>
2018-06-28 14:23:39 -0700

    update configureStore.prod

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,17 +1,41 @@
 // @flow
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createHashHistory } from 'history';
+import { hashHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
+import {
+  forwardToMain,
+  forwardToRenderer,
+  triggerAlias,
+  replayActionMain,
+  replayActionRenderer
+} from 'electron-redux';
 import rootReducer from '../reducers';
 import type { counterStateType } from '../reducers/counter';
 
-const history = createHashHistory();
-const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
+const configureStore = (initialState?: counterStateType, scope = 'main') => {
+  let middleware = [thunk];
 
-function configureStore(initialState?: counterStateType) {
-  return createStore(rootReducer, initialState, enhancer);
-}
+  if (scope === 'renderer') {
+    const router = routerMiddleware(hashHistory);
+    middleware = [forwardToMain, router, ...middleware];
+  }
 
-export default { configureStore, history };
+  if (scope === 'main') {
+    middleware = [triggerAlias, ...middleware, forwardToRenderer];
+  }
+
+  const enhancer = applyMiddleware(...middleware);
+
+  const store = createStore(rootReducer, initialState, enhancer);
+
+  if (scope === 'main') {
+    replayActionMain(store);
+  } else {
+    replayActionRenderer(store);
+  }
+
+  return store;
+};
+
+export default { configureStore };
