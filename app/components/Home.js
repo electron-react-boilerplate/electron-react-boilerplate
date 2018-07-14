aa6194c8acc8b758bb1d46497428969267c17fba
Adam Goldman <goldy@adamgoldman.me>
2018-07-14 23:33:06 +0300

    refactor routes to use constants
    - rename `routes.js` to `Routes.js` (since it exposes a `React.Component`)
    - export const routes from `constants/routes.js`
    - use const routes instead of hard coded through the app

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as routes from '../constants/routes';
 import styles from './Home.css';
 
 type Props = {};
@@ -13,7 +14,7 @@ export default class Home extends Component<Props> {
       <div>
         <div className={styles.container} data-tid="container">
           <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+          <Link to={routes.COUNTER}>to Counter</Link>
         </div>
       </div>
     );
---
+++ app/constants/routes.js
@@ -0,0 +1,2 @@
+export const HOME = '/';
+export const COUNTER = '/counter';
