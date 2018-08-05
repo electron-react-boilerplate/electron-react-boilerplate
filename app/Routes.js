7ba927199c609dd7cbbe1e0ff12d573936724949
Amila Welihinda <amilajack@gmail.com>
2018-08-04 21:20:41 -0700

    Add route based code splitting

--- app/Routes.js
+++ app/Routes.js
@@ -1,16 +1,40 @@
 /* eslint flowtype-errors/show-errors: 0 */
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import Loadable from 'react-loadable';
 import routes from './constants/routes.json';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+
+export function LoadableHelper(opts = {}) {
+  return Loadable(
+    Object.assign(
+      {
+        loading: <div>Loading component</div>,
+        delay: 200,
+        timeout: 10
+      },
+      opts
+    )
+  );
+}
 
 export default () => (
   <App>
     <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.HOME} component={HomePage} />
+      <Route
+        key={Math.random()}
+        path={routes.COUNTER}
+        component={LoadableHelper({
+          loader: () => import('./containers/CounterPage')
+        })}
+      />
+      <Route
+        key={Math.random()}
+        path={routes.HOME}
+        component={LoadableHelper({
+          loader: () => import('./containers/HomePage')
+        })}
+      />
     </Switch>
   </App>
 );
