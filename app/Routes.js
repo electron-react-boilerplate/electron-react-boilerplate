b553c2d26995e2c8a62d7cef09f12b1cef161f99
vikr01 <vikr01@users.noreply.github.com>
2018-08-20 21:19:33 -0700

    resolve json/jsx paths

--- app/Routes.js
+++ app/Routes.js
@@ -1,7 +1,7 @@
 /* eslint flowtype-errors/show-errors: 0 */
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import routes from './constants/routes.json';
+import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
