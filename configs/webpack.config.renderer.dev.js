62e8e35ff6c9ba6a9d2a1879a3a1c35a659f9940
vikr01 <vik120@icloud.com>
2018-09-06 20:58:14 -0700

    fix babel plugins specified in webpack config

--- configs/webpack.config.renderer.dev.js
+++ configs/webpack.config.renderer.dev.js
@@ -70,8 +70,8 @@ export default merge.smart(baseConfig, {
               // Here, we include babel plugins that are only required for the
               // renderer process. The 'transform-*' plugins must be included
               // before react-hot-loader/babel
-              'transform-class-properties',
-              'transform-es2015-classes',
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-transform-classes',
               'react-hot-loader/babel'
             ]
           }
