b4a41f0c427a7801ddd247542774ec58e1229a57
Amila Welihinda <amilajack@gmail.com>
2018-09-07 00:36:10 -0700

    Fix electron-rebuild postinstall script

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,8 @@ module.exports = {
       '@babel/preset-env',
       {
         targets: { node: 10 },
-        useBuiltIns: 'usage'
+        useBuiltIns: 'usage',
+        modules: 'commonjs'
       }
     ],
     '@babel/preset-flow'
