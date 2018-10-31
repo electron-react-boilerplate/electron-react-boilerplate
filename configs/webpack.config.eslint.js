7c644c1d1cfc097d23f259b5f5da79f5b50a6a03
Vikram Rangaraj <vik120@icloud.com>
2018-10-31 11:50:01 -0700

    Use .babel.js for webpack instead of using babel register (#1908)

--- configs/webpack.config.eslint.js
+++ configs/webpack.config.eslint.js
@@ -1,4 +1,4 @@
 /* eslint import/no-unresolved: off, import/no-self-import: off */
 require('@babel/register');
 
-module.exports = require('./webpack.config.renderer.dev').default;
+module.exports = require('./webpack.config.renderer.dev.babel').default;
