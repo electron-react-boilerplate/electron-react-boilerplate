d49f46306d1e6c319c03c4a896d77189e8304ddb
Amila Welihinda <amilajack@gmail.com>
2018-11-08 20:24:18 -0800

    fix package size bug

--- configs/webpack.config.base.js
+++ configs/webpack.config.base.js
@@ -4,11 +4,8 @@
 
 import path from 'path';
 import webpack from 'webpack';
-import { dependencies } from '../package.json';
 
 export default {
-  externals: [...Object.keys(dependencies || {})],
-
   module: {
     rules: [
       {
