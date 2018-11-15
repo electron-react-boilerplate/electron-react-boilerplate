3e6920a38e7399183850891badf9a9e41f12801c
Amila Welihinda <amilajack@gmail.com>
2018-11-14 22:57:17 -0800

    bump deps, fix breaking changes for connect-react-router

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,10 +3,9 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import counter from './counter';
 
-export default function createRootReducer(history: {}) {
-  const routerReducer = connectRouter(history)(() => {});
-
-  return connectRouter(history)(
-    combineReducers({ router: routerReducer, counter })
-  );
+export default function createRootReducer(history: History) {
+  return combineReducers({
+    router: connectRouter(history),
+    counter
+  });
 }
