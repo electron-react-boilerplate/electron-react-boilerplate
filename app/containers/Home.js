858555f1a459e4d748f0f7dba04897cceef8b942
Adam Tay <adamxnzl@gmail.com>
2019-01-29 21:35:39 +1300

    Adding emotion-js

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,12 +1,19 @@
+/** @jsx jsx */
 import * as React from 'react';
+import { jsx, css } from '@emotion/core';
 import DevTools from 'mobx-react-devtools';
 
 export default class HomePage extends React.Component {
   render() {
+    const styles = css({
+      display: 'flex',
+      justifyContent: 'center'
+    });
+
     return (
       <div>
         {process.env.NODE_ENV === 'development' ? <DevTools /> : null}
-        <div>Electron-Mobx-Boilerplate</div>
+        <div css={styles}>Electron-Mobx-Boilerplate</div>
       </div>
     );
   }
