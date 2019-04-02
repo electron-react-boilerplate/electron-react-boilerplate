546101034275d887e92d480b6405590d596f06aa
Andy Edwards <jedwards@fastmail.com>
2019-04-02 11:40:13 -0500

    add PLAIN_HMR environment variable option (#2153)
    * build: `PLAIN_HMR=1 yarn dev` will disable `react-hot-loader`
    
    * Update app/index.js
    
    Co-Authored-By: jedwards1211 <jedwards@fastmail.com>

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 
 const store = configureStore();
+
+const AppContainer = process.env.PLAIN_HMR
+  ? React.Fragment
+  : ReactHotAppContainer;
 
 render(
   <AppContainer>
