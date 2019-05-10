6d55865ff47e2cbf441bf0962fd39fb6408579d5
akshay <akshayjpatil11@gmail.com>
2019-05-09 22:53:58 -0400

    Added route level splitting with react-loadable

--- app/Routes.js
+++ app/Routes.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import Loadable from 'react-loadable';
+
 import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+
+const CounterPageComponent = Loadable({
+  loader: () => import('./containers/CounterPage'),
+  loading: () => <div>Loading...</div>
+});
 
 export default () => (
   <App>
     <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
+      <Route path={routes.COUNTER} component={CounterPageComponent} />
       <Route path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
