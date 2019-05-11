aa7162572f93ea940f3ec50aa0dd3258e2e90fb9
akshay <akshayjpatil11@gmail.com>
2019-05-11 14:10:11 -0400

    Added type for history to resolve flow error

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,10 @@
 // @flow
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
+import { IHashHistory } from 'history';
 import counter from './counter';
 
-export default function createRootReducer(history: History) {
+export default function createRootReducer(history: IHashHistory) {
   return combineReducers({
     router: connectRouter(history),
     counter
