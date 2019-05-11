f19414af71a8f8e9b88480abda634cf14098239c
akshay <akshayjpatil11@gmail.com>
2019-05-11 16:42:15 -0400

    Trying code splitting with React.Lazy

--- app/Routes.js
+++ app/Routes.js
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router';
-import Loadable from 'react-loadable';
-
 import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 
-const CounterPageComponent = Loadable({
-  loader: () => import('./containers/CounterPage'),
-  loading: () => <div>Loading...</div>
-});
+const CounterPageComponent = lazy(() => import('./containers/CounterPage'));
 
 export default () => (
   <App>
-    <Switch>
-      <Route path={routes.COUNTER} component={CounterPageComponent} />
-      <Route path={routes.HOME} component={HomePage} />
-    </Switch>
+    <Suspense fallback={() => <div>Loading...</div>}>
+      <Switch>
+        <Route path={routes.COUNTER} component={CounterPageComponent} />
+        <Route path={routes.HOME} component={HomePage} />
+      </Switch>
+    </Suspense>
   </App>
 );
