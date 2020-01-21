758603c2aa542f932a1c723a0b4de1df98d3ad42
Amila Welihinda <amilajack@gmail.com>
2020-01-21 14:57:05 -0800

    remove all .map and .js files from emitted ts output

--- app/main.dev.babel.js
+++
@@ -1,7 +0,0 @@
-const register = require('@babel/register').default;
-
-register({
-  extensions: ['.ts', '.tsx', '.js', '.json']
-});
-
-require('./main.dev.ts');
--- app/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import configureStoreDev from './configureStore.dev';
-import configureStoreProd from './configureStore.prod';
-var selectedConfigureStore =
-  process.env.NODE_ENV === 'production'
-    ? configureStoreProd
-    : configureStoreDev;
-export var configureStore = selectedConfigureStore.configureStore;
-export var history = selectedConfigureStore.history;
-//# sourceMappingURL=configureStore.js.map
