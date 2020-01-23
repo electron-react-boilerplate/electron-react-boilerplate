0bc0dd22f8af7aa587a7ac65305e31dab1f7b90f
Amila Welihinda <amilajack@gmail.com>
2020-01-23 12:14:49 -0800

    remove jsx entry from renderer dev

--- configs/webpack.config.renderer.dev.babel.js
+++ configs/webpack.config.renderer.dev.babel.js
@@ -63,16 +63,6 @@ export default merge.smart(baseConfig, {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true
-          }
-        }
-      },
-      {
         test: /\.global\.css$/,
         use: [
           {
