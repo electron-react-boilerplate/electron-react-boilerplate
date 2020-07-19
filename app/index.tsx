922c584af5627642c5fcb46bcf92848f75e2c9e1
Justin Murray <justin@murrayju.com>
2020-07-19 13:13:11 -0600

    Fix for preload entrypoint (#2503)
    Fixes #2395.
    
    I believe that this is a proper fix, and not just a workaround. This probably requires validation from someone more knowledgeable.

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
-import Root from './containers/Root';
 import { history, configuredStore } from './store';
 import './app.global.css';
 
@@ -9,11 +8,13 @@ const store = configuredStore();
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
-document.addEventListener('DOMContentLoaded', () =>
+document.addEventListener('DOMContentLoaded', () => {
+  // eslint-disable-next-line global-require
+  const Root = require('./containers/Root').default;
   render(
     <AppContainer>
       <Root store={store} history={history} />
     </AppContainer>,
     document.getElementById('root')
-  )
-);
+  );
+});
