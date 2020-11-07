cd537977e072df429a5ad16ecfb7b8e93a255642
Amila Welihinda <amilajack@gmail.com>
2020-11-07 14:35:39 -0800

    chore: rename resources dir to assets

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -84,7 +84,7 @@ const createWindow = async () => {
           },
   });
 
-  mainWindow.loadURL(`file://${__dirname}/app.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // @TODO: Use 'ready-to-show' event
   //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
