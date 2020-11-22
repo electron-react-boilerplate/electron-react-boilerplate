6c22509ca6918e57980abb3232a7aa8fffd82354
Amila Welihinda <amilajack@gmail.com>
2020-11-22 12:59:18 -0800

    feat: move dll dir to .erb dir

--- .erb/configs/webpack.config.renderer.dev.babel.js
+++ .erb/configs/webpack.config.renderer.dev.babel.js
@@ -23,8 +23,8 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 1212;
 const publicPath = `http://localhost:${port}/dist`;
-const dll = path.join(__dirname, '../../dll');
-const manifest = path.resolve(dll, 'renderer.json');
+const dllDir = path.join(__dirname, '../dll');
+const manifest = path.resolve(dllDir, 'renderer.json');
 const requiredByDLLConfig = module.parent.filename.includes(
   'webpack.config.renderer.dev.dll'
 );
@@ -32,7 +32,7 @@ const requiredByDLLConfig = module.parent.filename.includes(
 /**
  * Warn if the DLL is not built
  */
-if (!requiredByDLLConfig && !(fs.existsSync(dll) && fs.existsSync(manifest))) {
+if (!requiredByDLLConfig && !(fs.existsSync(dllDir) && fs.existsSync(manifest))) {
   console.log(
     chalk.black.bgYellow.bold(
       'The DLL files are missing. Sit back while we build them for you with "yarn build-dll"'
--- .erb/configs/webpack.config.renderer.dev.dll.babel.js
+++ .erb/configs/webpack.config.renderer.dev.dll.babel.js
@@ -11,7 +11,7 @@ import CheckNodeEnv from '../scripts/CheckNodeEnv';
 
 CheckNodeEnv('development');
 
-const dist = path.join(__dirname, '../../dll');
+const dist = path.join(__dirname, '../dll');
 
 export default merge(baseConfig, {
   context: path.join(__dirname, '../..'),
@@ -64,7 +64,7 @@ export default merge(baseConfig, {
       options: {
         context: path.join(__dirname, '../../src'),
         output: {
-          path: path.join(__dirname, '../../dll'),
+          path: path.join(__dirname, '../dll'),
         },
       },
     }),
