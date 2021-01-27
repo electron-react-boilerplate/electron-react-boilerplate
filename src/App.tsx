d1ac9b1e8e511c68e0146e712ce7263a9a8bfc37
Amila Welihinda <amilajack@gmail.com>
2021-01-26 16:05:00 -0800

    feat: minor improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/icon.svg';
+import './App.global.css';
 
 const Hello = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import './App.global.css';
 
 render(<App />, document.getElementById('root'));
