f4e9163cf29083e90556b7f32f8203b9dc7d9db7
shot <sht04052@hotmail.com>
2021-04-19 16:42:19 -0500

    Updated app test for jest.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,6 +3,16 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
+Object.defineProperty(window, 'api', {
+  writable: false,
+  value: {
+    incrementNumber: jest.fn(),
+    decrementNumber: jest.fn(),
+    onResponse: jest.fn(),
+    removeResponseChannel: jest.fn(),
+  },
+});
+
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
