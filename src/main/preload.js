d2385da0299b5cf515c7bc36be0f93191aa757a1
Amila Welihinda <amilajack@gmail.com>
2021-05-05 19:48:28 -0700

    fix: remove inaccurate comment from preload.js

--- src/main/preload.js
+++ src/main/preload.js
@@ -4,13 +4,6 @@ contextBridge.exposeInMainWorld('electron', {
   ipcRenderer: {
     send: ipcRenderer.send,
     sendSync: ipcRenderer.sendSync,
-    /**
-     * You are still able to use ipcRenderer in a renderer process with contextIsolation set to true.
-     * The contextBridge is what you want to use, although there is a current bug that is preventing from you calling ipcRenderer.on in a renderer process;
-     * all you can do is send from the renderer process to the main process.
-     * This code is taken from secure-electron-template a template for Electron built with security in mind.
-     * You can find more info about this over here: https://stackoverflow.com/questions/55164360/with-contextisolation-true-is-it-possible-to-use-ipcrenderer
-     */
     on(channel, func) {
       const validChannels = ['ipc-example'];
       if (validChannels.includes(channel)) {
