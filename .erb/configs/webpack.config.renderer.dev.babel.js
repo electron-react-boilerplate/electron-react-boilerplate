a5b885640cbe7a7d7d7e9c2b2c172a6d4eb1cae2
Amila Welihinda <amilajack@gmail.com>
2021-06-22 18:51:34 -0700

    fix: add dev server entrypoints to fix hmr

--- .erb/configs/webpack.config.renderer.dev.babel.js
+++ .erb/configs/webpack.config.renderer.dev.babel.js
@@ -26,7 +26,10 @@ const requiredByDLLConfig = module.parent.filename.includes(
 /**
  * Warn if the DLL is not built
  */
-if (!requiredByDLLConfig && !(fs.existsSync(webpackPaths.dllPath) && fs.existsSync(manifest))) {
+if (
+  !requiredByDLLConfig &&
+  !(fs.existsSync(webpackPaths.dllPath) && fs.existsSync(manifest))
+) {
   console.log(
     chalk.black.bgYellow.bold(
       'The DLL files are missing. Sit back while we build them for you with "yarn build-dll"'
@@ -43,6 +46,8 @@ export default merge(baseConfig, {
   target: ['web', 'electron-renderer'],
 
   entry: [
+    'webpack-dev-server/client?http://localhost:1212/dist',
+    'webpack/hot/only-dev-server',
     'core-js',
     'regenerator-runtime/runtime',
     path.join(webpackPaths.srcRendererPath, 'index.tsx'),
@@ -54,7 +59,7 @@ export default merge(baseConfig, {
     filename: 'renderer.dev.js',
     library: {
       type: 'umd',
-    }
+    },
   },
 
   module: {
@@ -66,9 +71,7 @@ export default merge(baseConfig, {
           {
             loader: require.resolve('babel-loader'),
             options: {
-              plugins: [
-                require.resolve('react-refresh/babel'),
-              ].filter(Boolean),
+              plugins: [require.resolve('react-refresh/babel')].filter(Boolean),
             },
           },
         ],
@@ -216,7 +219,6 @@ export default merge(baseConfig, {
     ],
   },
   plugins: [
-
     requiredByDLLConfig
       ? null
       : new webpack.DllReferencePlugin({
@@ -290,13 +292,13 @@ export default merge(baseConfig, {
     },
     before() {
       console.log('Starting Main Process...');
-        spawn('npm', ['run', 'start:main'], {
-          shell: true,
-          env: process.env,
-          stdio: 'inherit',
-        })
-          .on('close', (code) => process.exit(code))
-          .on('error', (spawnError) => console.error(spawnError));
+      spawn('npm', ['run', 'start:main'], {
+        shell: true,
+        env: process.env,
+        stdio: 'inherit',
+      })
+        .on('close', (code) => process.exit(code))
+        .on('error', (spawnError) => console.error(spawnError));
     },
   },
 });
