9e91c3a6335222014e3070f735f0f2b02cc9c01a
Amila Welihinda <amilajack@gmail.com>
2021-08-19 14:23:23 -0700

    fix: symlink node_modules with junction points on windows

--- .erb/scripts/link-modules.js
+++ .erb/scripts/link-modules.js
@@ -5,5 +5,5 @@ import {
 } from '../configs/webpack.paths.js';
 
 if (!fs.existsSync(srcNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
-  fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath);
+  fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
 }
