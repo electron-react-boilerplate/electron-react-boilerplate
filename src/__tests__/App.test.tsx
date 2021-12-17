276d1789a886f15326d4e4fd05dc2142a7ed5477
Ole Tjernstad <ott556@gmail.com>
2021-12-17 22:23:20 +0100

    add material and remove test scripts

--- src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import App from '../renderer/App';
-
-describe('App', () => {
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
-  });
-});
