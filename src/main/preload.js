88b132b1bf6e58b3e5711297f714f66ab29a5fe6
Parag Baxi <ghee22@gmail.com>
2022-01-25 21:10:31 -0500

    main to renderer example

--- src/main/preload.js
+++ src/main/preload.js
@@ -5,15 +5,18 @@ contextBridge.exposeInMainWorld('electron', {
     myPing() {
       ipcRenderer.send('ipc-example', 'ping');
     },
+    mainToRendererExample(message) {
+      ipcRenderer.send('main-to-renderer-example', 'table tennis');
+    },
     on(channel, func) {
-      const validChannels = ['ipc-example'];
+      const validChannels = ['ipc-example','main-to-renderer-example'];
       if (validChannels.includes(channel)) {
         // Deliberately strip event as it includes `sender`
         ipcRenderer.on(channel, (event, ...args) => func(...args));
       }
     },
     once(channel, func) {
-      const validChannels = ['ipc-example'];
+      const validChannels = ['ipc-example','main-to-renderer-example'];
       if (validChannels.includes(channel)) {
         // Deliberately strip event as it includes `sender`
         ipcRenderer.once(channel, (event, ...args) => func(...args));
