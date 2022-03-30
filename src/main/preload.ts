aa16d2f2e18ee0046800a7cf0791d7b5f163f007
Nexcontrol <bhs2874@naver.com>
2022-03-30 18:03:54 +0900

    #3176, change type 'any' to 'unknown' and more
    * ChangeLog
    * 1. change tpye 'any' to 'unknown' in 'preload.ts' and 'preload.d.ts'.
    * 2. add example 'removeListener' in 'preload.ts' and change 'on' type in 'preload.d.ts' according to 'preload.ts'.
    
    * close: #3176

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,20 +1,24 @@
-import { contextBridge, ipcRenderer } from 'electron';
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
 contextBridge.exposeInMainWorld('electron', {
   ipcRenderer: {
     myPing() {
       ipcRenderer.send('ipc-example', 'ping');
     },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    on(channel: string, func: (...args: any[]) => void) {
+    on(channel: string, func: (...args: unknown[]) => void) {
       const validChannels = ['ipc-example'];
       if (validChannels.includes(channel)) {
+        const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
+          func(...args);
         // Deliberately strip event as it includes `sender`
-        ipcRenderer.on(channel, (_event, ...args) => func(...args));
+        ipcRenderer.on(channel, subscription);
+
+        return () => ipcRenderer.removeListener(channel, subscription);
       }
+
+      return undefined;
     },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    once(channel: string, func: (...args: any[]) => void) {
+    once(channel: string, func: (...args: unknown[]) => void) {
       const validChannels = ['ipc-example'];
       if (validChannels.includes(channel)) {
         // Deliberately strip event as it includes `sender`
--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -3,10 +3,11 @@ declare global {
     electron: {
       ipcRenderer: {
         myPing(): void;
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        on(channel: string, func: (...args: any[]) => void): void;
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        once(channel: string, func: (...args: any[]) => void): void;
+        on(
+          channel: string,
+          func: (...args: unknown[]) => void
+        ): (() => void) | undefined;
+        once(channel: string, func: (...args: unknown[]) => void): void;
       };
     };
   }
