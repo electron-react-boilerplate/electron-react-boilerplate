07f232c26d33e07a917252f1b08910a2e52235f5
Amila Welihinda <amilajack@gmail.com>
2022-04-05 11:20:29 -0700

    chore: bump deps

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,9 @@
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<App />);
 
 // calling IPC exposed from preload script
 window.electron.ipcRenderer.once('ipc-example', (arg) => {
