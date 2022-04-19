5d54ba3614e7ee566a201d50f7649522c6266f12
Amila Welihinda <amilajack@gmail.com>
2022-04-19 10:43:05 -0700

    refactor: rename isDevelopment to isDebug

--- src/main/main.ts
+++ src/main/main.ts
@@ -36,10 +36,10 @@ if (process.env.NODE_ENV === 'production') {
   sourceMapSupport.install();
 }
 
-const isDevelopment =
+const isDebug =
   process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
-if (isDevelopment) {
+if (isDebug) {
   require('electron-debug')();
 }
 
@@ -57,7 +57,7 @@ const installExtensions = async () => {
 };
 
 const createWindow = async () => {
-  if (isDevelopment) {
+  if (isDebug) {
     await installExtensions();
   }
 
