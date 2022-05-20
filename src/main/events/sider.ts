19e2de066c3486384cb795d8cdf4c16485fca947
zhaogongchengsi <zzh1586169624@163.com>
2022-05-20 22:26:46 +0800

    更新 侧边栏事件监听

--- src/main/events/sider.ts
+++ src/main/events/sider.ts
@@ -1,10 +1,13 @@
 import { BrowserWindow, ipcMain } from 'electron';
+import { Pool } from '../connectPool';
 
 // 监听侧边栏事件
 export default class SiderEvent {
   window: BrowserWindow;
-  constructor(win: BrowserWindow) {
+  connectPool: Pool
+  constructor(win: BrowserWindow, connectPool: Pool) {
     this.window = win;
+    this.connectPool = connectPool;
   }
 
 
