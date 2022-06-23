b32c5627fc0c2cac87b4c92384e3af7d965731ca
nedsociety <nedsociety@users.noreply.github.com>
2022-06-24 07:45:13 +0900

    fix: vscode debugging (electron-react-boilerplate#428) (#3169)

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -172,7 +172,13 @@ const configuration: webpack.Configuration = {
         .on('error', (spawnError) => console.error(spawnError));
 
       console.log('Starting Main Process...');
-      spawn('npm', ['run', 'start:main'], {
+      let args = ['run', 'start:main'];
+      if (process.env.MAIN_ARGS) {
+        args = args.concat(
+          ['--', ...process.env.MAIN_ARGS.matchAll(/"[^"]+"|[^\s"]+/g)].flat()
+        );
+      }
+      spawn('npm', args, {
         shell: true,
         stdio: 'inherit',
       })
