7511174cd1982d43b2ed19a14a27dd2528e0245a
Konstantin Komelin <konstantin.komelin@gmail.com>
2022-07-27 21:48:20 +0300

    fix: Wrong img[width] units in the example app (#3246)

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,7 +6,7 @@ const Hello = () => {
   return (
     <div>
       <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
+        <img width="200" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
       <div className="Hello">
