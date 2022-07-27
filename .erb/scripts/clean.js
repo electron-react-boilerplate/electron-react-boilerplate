f6737bd5e3cea83deeed2c27e0f726a267758be8
Pavel Penkov <Ciberus.ps@gmail.com>
2022-07-27 21:49:15 +0300

    fix clean script (#3267)
    * fix clean script
    
    * Update clean.js
    
    Co-authored-by: Amila Welihinda <amilajack@gmail.com>

--- .erb/scripts/clean.js
+++ .erb/scripts/clean.js
@@ -2,16 +2,12 @@ import rimraf from 'rimraf';
 import process from 'process';
 import webpackPaths from '../configs/webpack.paths';
 
-const args = process.argv.slice(2);
-const commandMap = {
-  dist: webpackPaths.distPath,
-  release: webpackPaths.releasePath,
-  dll: webpackPaths.dllPath,
-};
+const foldersToRemove = [
+  webpackPaths.distPath,
+  webpackPaths.buildPath,
+  webpackPaths.dllPath,
+];
 
-args.forEach((x) => {
-  const pathToRemove = commandMap[x];
-  if (pathToRemove !== undefined) {
-    rimraf.sync(pathToRemove);
-  }
+foldersToRemove.forEach((folder) => {
+  rimraf.sync(folder);
 });
