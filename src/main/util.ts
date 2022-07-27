ccb801ecc948cf1c7074e6541634aba321168fa4
Łukasz Rabiec <lukaszrabiec@gmail.com>
2022-07-27 20:52:01 +0200

    refactored resolveHtmlPath (#3253)

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,18 +1,13 @@
-/* eslint import/prefer-default-export: off, import/no-mutable-exports: off */
+/* eslint import/prefer-default-export: off */
 import { URL } from 'url';
 import path from 'path';
 
-export let resolveHtmlPath: (htmlFileName: string) => string;
-
-if (process.env.NODE_ENV === 'development') {
-  const port = process.env.PORT || 1212;
-  resolveHtmlPath = (htmlFileName: string) => {
+export function resolveHtmlPath(htmlFileName: string) {
+  if (process.env.NODE_ENV === 'development') {
+    const port = process.env.PORT || 1212;
     const url = new URL(`http://localhost:${port}`);
     url.pathname = htmlFileName;
     return url.href;
-  };
-} else {
-  resolveHtmlPath = (htmlFileName: string) => {
-    return `file://${path.resolve(__dirname, '../renderer/', htmlFileName)}`;
-  };
+  }
+  return `file://${path.resolve(__dirname, '../renderer/', htmlFileName)}`;
 }
