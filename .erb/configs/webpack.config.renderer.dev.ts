4114be8f648f183946ea8f3b9149b2f304a09eae
Amila Welihinda <amilajack@gmail.com>
2022-08-16 16:10:33 -0700

    fix code styles

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -20,8 +20,8 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 1212;
 const manifest = path.resolve(webpackPaths.dllPath, 'renderer.json');
 const skipDLLs =
-  module.parent?.filename.includes("webpack.config.renderer.dev.dll") ||
-  module.parent?.filename.includes("webpack.config.eslint")
+  module.parent?.filename.includes('webpack.config.renderer.dev.dll') ||
+  module.parent?.filename.includes('webpack.config.eslint');
 
 /**
  * Warn if the DLL is not built
