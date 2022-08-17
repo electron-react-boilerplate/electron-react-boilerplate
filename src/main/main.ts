dec96b9187992d7872dce40ece9fa721873e4ee9
Amila Welihinda <amilajack@gmail.com>
2022-08-17 11:49:50 -0700

    chore: bump deps (#3291)
    * chore: bump deps
    
    * fix: disable browserwindow sandbox

--- src/main/main.ts
+++ src/main/main.ts
@@ -75,6 +75,7 @@ const createWindow = async () => {
     height: 728,
     icon: getAssetPath('icon.png'),
     webPreferences: {
+      sandbox: false,
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
