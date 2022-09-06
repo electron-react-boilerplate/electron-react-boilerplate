00f8f1965cb99c60593aa071322fa32962fe2f91
Shyamkumar Yadav <shyamkumaryadav2003@gmail.com>
2022-09-06 15:58:52 +0530

    add support of SVG as Component #3307

--- .erb/configs/webpack.config.renderer.prod.ts
+++ .erb/configs/webpack.config.renderer.prod.ts
@@ -66,8 +66,27 @@ const configuration: webpack.Configuration = {
       },
       // Images
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+      },
+      // SVG
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              prettier: false,
+              svgo: false,
+              svgoConfig: {
+                plugins: [{ removeViewBox: false }],
+              },
+              titleProp: true,
+              ref: true,
+            },
+          },
+          'file-loader',
+        ],
       },
     ],
   },
