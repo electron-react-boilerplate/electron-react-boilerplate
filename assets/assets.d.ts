c07c2ef891851a181ed77fe750beb1b81aedffc8
Shyamkumar Yadav <46707211+shyamkumaryadav@users.noreply.github.com>
2022-09-06 20:47:30 +0530

    add support of SVG as Component #3307 (#3308)

--- assets/assets.d.ts
+++ assets/assets.d.ts
@@ -1,6 +1,8 @@
 type Styles = Record<string, string>;
 
 declare module '*.svg' {
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+
   const content: string;
   export default content;
 }
