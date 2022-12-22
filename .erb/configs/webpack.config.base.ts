40dcb008b6ac796f0799e05ddf96378761465775
Amaral Krichman <amaralkrichman@gmail.com>
2022-12-22 17:21:58 -0300

    add: tsconfig-paths-webpack-plugin

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -3,6 +3,7 @@
  */
 
 import webpack from 'webpack';
+import TsconfigPathsPlugins from 'tsconfig-paths-webpack-plugin';
 import webpackPaths from './webpack.paths';
 import { dependencies as externals } from '../../release/app/package.json';
 
@@ -44,6 +45,8 @@ const configuration: webpack.Configuration = {
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
     modules: [webpackPaths.srcPath, 'node_modules'],
+    // There is no need to add aliases here, the paths in tsconfig get mirrored
+    plugins: [new TsconfigPathsPlugins()],
   },
 
   plugins: [
