2be8c2d3a39e74625fc747c730763458cd165c8f
nfray <harry.fray7@gmail.com>
2023-02-21 16:25:52 -0700

    style updates

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,8 +71,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 600,
+    height: 500,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       // TODO: UPDATE ONCE OUT OF DEVELOPMENT
