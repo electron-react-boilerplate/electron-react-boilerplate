8b123a0c6217eec30ab8b39ebbc82c317e57ee6c
Shyamkumar Yadav <46707211+shyamkumaryadav@users.noreply.github.com>
2023-04-15 02:26:53 +0530

    fix: Forbidden non-null assertion (#3440)

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root')!;
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
 
