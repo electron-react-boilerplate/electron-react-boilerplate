671d2fce073f6f8bd3f42e65b8174b28f1461341
Georgian Camarasan <georgian.camarasan@zohomail.eu>
2023-04-19 22:09:28 +0300

    fix: added glob option to rimraf.sync call

--- .erb/scripts/delete-source-maps.js
+++ .erb/scripts/delete-source-maps.js
@@ -5,7 +5,11 @@ import webpackPaths from '../configs/webpack.paths';
 
 export default function deleteSourceMaps() {
   if (fs.existsSync(webpackPaths.distMainPath))
-    rimraf.sync(path.join(webpackPaths.distMainPath, '*.js.map'));
+    rimraf.sync(path.join(webpackPaths.distMainPath, '*.js.map'), {
+      glob: true,
+    });
   if (fs.existsSync(webpackPaths.distRendererPath))
-    rimraf.sync(path.join(webpackPaths.distRendererPath, '*.js.map'));
+    rimraf.sync(path.join(webpackPaths.distRendererPath, '*.js.map'), {
+      glob: true,
+    });
 }
