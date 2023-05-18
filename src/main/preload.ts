1cbbb44dfadd8b81bcf3553fca344065f1ecf70c
moonrailgun <moonrailgun@gmail.com>
2023-05-19 05:57:24 +0800

    fix: fix ipcRenderer.sendMessage incorrect defintion (#3436)
    In electron origin definition with `send` should be allow accept multi args

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -6,8 +6,8 @@ export type Channels = 'ipc-example';
 
 const electronHandler = {
   ipcRenderer: {
-    sendMessage(channel: Channels, args: unknown[]) {
-      ipcRenderer.send(channel, args);
+    sendMessage(channel: Channels, ...args: unknown[]) {
+      ipcRenderer.send(channel, ...args);
     },
     on(channel: Channels, func: (...args: unknown[]) => void) {
       const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
