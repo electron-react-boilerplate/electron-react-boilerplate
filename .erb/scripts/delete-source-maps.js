c0136ae7e955b1c4449b3fc57efc57263b91a0cb
Amila Welihinda <amilajack@gmail.com>
2023-06-03 19:05:50 -0700

    chore: bump deps (#3486)

--- .erb/scripts/delete-source-maps.js
+++ .erb/scripts/delete-source-maps.js
@@ -1,15 +1,15 @@
 import fs from 'fs';
 import path from 'path';
-import rimraf from 'rimraf';
+import { rimrafSync } from 'rimraf';
 import webpackPaths from '../configs/webpack.paths';
 
 export default function deleteSourceMaps() {
   if (fs.existsSync(webpackPaths.distMainPath))
-    rimraf.sync(path.join(webpackPaths.distMainPath, '*.js.map'), {
+    rimrafSync(path.join(webpackPaths.distMainPath, '*.js.map'), {
       glob: true,
     });
   if (fs.existsSync(webpackPaths.distRendererPath))
-    rimraf.sync(path.join(webpackPaths.distRendererPath, '*.js.map'), {
+    rimrafSync(path.join(webpackPaths.distRendererPath, '*.js.map'), {
       glob: true,
     });
 }
