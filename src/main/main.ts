53bccb643e2f4271cd6590aa351d8cf34bb9a033
tuomaskivioja <tuomas@tuomaskivioja.com>
2023-06-05 14:48:38 +0100

    initial

--- src/main/main.ts
+++ src/main/main.ts
@@ -15,6 +15,8 @@ import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 
+// hello
+
 class AppUpdater {
   constructor() {
     log.transports.file.level = 'info';
