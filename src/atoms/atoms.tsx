ad4c34c11aea1bf579a57725d5499d88577f5e9b
Coke Javier <lopezespinosjavier@gmail.com>
2023-06-14 18:15:12 +0200

    Initially select first item on data's array

--- src/atoms/atoms.tsx
+++ src/atoms/atoms.tsx
@@ -7,10 +7,15 @@ export const keywords = atom((get) => Object.keys(get(data)));
 export const contents = atom((get) => Object.entries(get(data)));
 
 //SideBar
-export const activeElement = atom<{ keyword: string; text: string }>({
-  keyword: '',
-  text: '',
-});
+export const initialElement = atom<{ keyword: string; text: string }>(
+  MockData[0]
+);
+export const changeElement = atom(
+  null,
+  (_get, set, value: { keyword: string; text: string }) => {
+    set(initialElement, value);
+  }
+);
 
 //NavBar
 export const tabSelected = atom('snippets');
--- src/renderer/sidebar/results-list/index.tsx
+++ src/renderer/sidebar/results-list/index.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as S from './results-list.styled';
 import { useAtom } from 'jotai';
-import { data, activeElement } from 'atoms/atoms';
+import { data, changeElement, initialElement } from 'atoms/atoms';
 
 const ResultsList: React.FC<{ search?: string }> = ({ search }) => {
   const [listData] = useAtom(data);
-  const [selected, setSelected] = useAtom(activeElement);
+  const [, selectNewItem] = useAtom(changeElement);
+  const [selected] = useAtom(initialElement);
   let newList = listData;
   if (search) {
     newList = listData.filter((element) => {
       const keywordMatch = element.keyword.toLowerCase().includes(search);
-      const textMatch = element.text.toLowerCase().includes(search);
+      // const textMatch = element.text.toLowerCase().includes(search);
 
-      return keywordMatch || textMatch;
+      return keywordMatch;
     });
   }
 
@@ -21,9 +22,9 @@ const ResultsList: React.FC<{ search?: string }> = ({ search }) => {
       {newList.length ? (
         newList.map((item) => (
           <S.ResultElement
-            onClick={() => setSelected(item)}
+            onClick={() => selectNewItem(item)}
             key={item.keyword}
-            selected={item.keyword === selected.keyword}
+            selected={item.keyword === selected?.keyword}
           >
             {item.keyword ?? '-'}
           </S.ResultElement>
