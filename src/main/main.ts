1391ce9a5add5f05fe85b337c0b7ec777a963aef
Amila Welihinda <amilajack@gmail.com>
2023-09-16 11:05:16 -0700

    fix: prettier errors

--- src/main/main.ts
+++ src/main/main.ts
@@ -51,7 +51,7 @@ const installExtensions = async () => {
   return installer
     .default(
       extensions.map((name) => installer[name]),
-      forceDownload
+      forceDownload,
     )
     .catch(console.log);
 };
