d274a9a49ec1b2c717e412a2300d989cd40de3f6
Coke Javier <lopezespinosjavier@gmail.com>
2023-09-18 19:23:15 +0200

    edit keyword and error control

--- src/renderer/sidebar/results-list/results-list.styled.ts
+++ src/renderer/sidebar/results-list/results-list.styled.ts
@@ -1,18 +1,40 @@
 import styled, { css } from 'styled-components';
-import { List } from 'rsuite';
+import { InputGroup, List } from 'rsuite';
+import EditIcon from '@rsuite/icons/Edit';
 
 const ListElement = css`
   display: flex;
-  padding: 8px 10px;
+  padding: 8px 8px 8px 10px;
   width: 100%;
+  justify-content: space-between;
+  align-items: flex-start;
 `;
 
-export const ResultsList = styled(List)`
-  color: ${({ theme }) => theme.gray_darker};
-  display: flex;
-  flex-direction: column;
-  margin-top: 0;
-  padding: 0;
+export const InputEdit = styled(InputGroup)`
+  &,
+  input {
+    outline: none !important;
+    border: none;
+    border-radius: 0;
+
+    &:focus,
+    &:hover {
+      border: none;
+      outline: none;
+    }
+  }
+`;
+
+export const Edit = styled(EditIcon)`
+  color: ${({ theme }) => theme.black};
+  padding: 4px;
+  min-width: 23px;
+
+  &:hover {
+    border-radius: 50%;
+    color: ${({ theme }) => theme.black};
+    background-color: ${({ theme }) => theme.primary_30};
+  }
 `;
 
 export const ResultElement = styled(List.Item)<{ selected: boolean }>`
@@ -25,6 +47,10 @@ export const ResultElement = styled(List.Item)<{ selected: boolean }>`
       ? css`
           background-color: ${theme.primary};
           color: ${({ theme }) => theme.white};
+
+          ${Edit} {
+            color: ${({ theme }) => theme.white};
+          }
         `
       : css`
           &:hover {
@@ -37,4 +63,35 @@ export const ResultElement = styled(List.Item)<{ selected: boolean }>`
 export const NoMatches = styled.span`
   ${ListElement}
   color: ${({ theme }) => theme.white};
+`;
+
+export const ResultsList = styled(List)<{ $error: boolean }>`
+  color: ${({ theme }) => theme.gray_darker};
+  display: flex;
+  flex-direction: column;
+  margin-top: 0;
+  padding: 0;
+
+  ${({ theme, $error }) =>
+    $error
+      ? css`
+          .rs-input-group.rs-input-group-inside {
+            .edition-input {
+              box-shadow: inset 0px 0px 0px 1px red;
+
+              &::placeholder {
+                color: ${theme.red} !important;
+              }
+            }
+            .rs-picker-toggle-placeholder {
+              color: ${theme.red} !important;
+            }
+            .rs-btn {
+              svg {
+                fill: ${theme.red} !important;
+              }
+            }
+          }
+        `
+      : ''}
 `;
--- src/renderer/sidebar/results-list/results-list.tsx
+++ src/renderer/sidebar/results-list/results-list.tsx
@@ -1,39 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './results-list.styled';
+import CheckRoundIcon from '@rsuite/icons/CheckRound';
+import { Input, InputGroup } from 'rsuite';
+import { SnippetType } from 'types/snippets';
 import { useAtom } from 'jotai';
 import {
   snippetsAtom,
   changeElementAtom,
   initialElementAtom,
 } from 'atoms/atoms';
-import { SnippetType } from 'types/snippets';
+
+type ResultListElement = SnippetType & {
+  edition: boolean;
+};
 
 const ResultsList: React.FC<{ search?: string }> = ({ search }) => {
-  const [listData] = useAtom(snippetsAtom);
+  const [listData, setListData] = useAtom(snippetsAtom);
   const [selected] = useAtom(initialElementAtom);
   const [, selectNewItem] = useAtom(changeElementAtom);
-  let newList = listData;
-  if (search) {
-    newList = listData.filter((element: SnippetType) => {
-      const keywordMatch = element.keyword.toLowerCase().includes(search);
-      const contentMatch = element.text.toLowerCase().includes(search);
+  let newList = listData.map((snippet) => {
+    return { ...snippet, edition: false };
+  });
+  const [isEditing, setIsEditing] = useState(
+    Array.from({ length: newList.length }, () => false)
+  );
+  const [editionError, setEditionError] = useState(false);
 
-      return keywordMatch || contentMatch;
+  if (search) {
+    newList = newList.filter((element: SnippetType) => {
+      const keywordMatch = element.keyword.toLowerCase().includes(search);
+
+      return keywordMatch;
     });
   }
 
+  const handleStartEditing = (index: number) => {
+    const newEdition = Array.from({ length: newList.length }, () => false);
+    newEdition[index] = true;
+    setIsEditing(newEdition);
+  };
+
+  const [editedList, setEditList] = useState<SnippetType[]>([]);
+  const handleEditKeyword = (newKeyword: string, snippet: SnippetType) => {
+    const match = listData
+      .map(
+        (element) =>
+          element.keyword === newKeyword && element.keyword !== snippet.keyword
+      )
+      .find((match) => match);
+    if (!newKeyword.trim() || match) {
+      setEditionError(true);
+    } else {
+      setEditionError(false);
+      const newEditedList = listData.map((element) => {
+        return {
+          ...element,
+          keyword:
+            element.keyword === snippet.keyword ? newKeyword : element.keyword,
+        };
+      });
+      setEditList(newEditedList);
+    }
+  };
+
+  const handleSaveKeyword = () => {
+    if (!editionError) {
+      setIsEditing(Array.from({ length: newList.length }, () => false));
+      editedList.length && setListData(editedList);
+    }
+  };
+
   return (
-    <S.ResultsList bordered={!!newList.length}>
+    <S.ResultsList $error={editionError} bordered={!!newList.length}>
       {newList.length ? (
-        newList.map((item: SnippetType) => (
-          <S.ResultElement
-            onClick={() => selectNewItem(item)}
-            key={item.keyword}
-            selected={item.keyword === selected?.keyword}
-          >
-            {item.keyword || '-'}
-          </S.ResultElement>
-        ))
+        newList.map((item: ResultListElement, index) =>
+          isEditing[index] ? (
+            <S.InputEdit inside key={item.keyword}>
+              <Input
+                className="edition-input"
+                name={`edit-${item.keyword}`}
+                defaultValue={item.keyword}
+                onChange={(newKeyword) => handleEditKeyword(newKeyword, item)}
+              />
+              <InputGroup.Button onClick={handleSaveKeyword}>
+                <CheckRoundIcon />
+              </InputGroup.Button>
+            </S.InputEdit>
+          ) : (
+            <S.ResultElement
+              onClick={() => selectNewItem(item)}
+              key={item.keyword}
+              selected={item.keyword === selected?.keyword}
+            >
+              {item.keyword || '-'}
+              <S.Edit onClick={() => handleStartEditing(index)} />
+            </S.ResultElement>
+          )
+        )
       ) : (
         <S.NoMatches>Sin resultados...</S.NoMatches>
       )}
