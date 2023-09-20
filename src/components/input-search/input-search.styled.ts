19caeb31fa2680862db38bdf7bfaf9a04ecbcb8b
Coke Javier <lopezespinosjavier@gmail.com>
2023-09-20 16:54:50 +0200

    Merge focus styles also with search input

--- src/components/input-search/input-search.styled.ts
+++ src/components/input-search/input-search.styled.ts
@@ -8,7 +8,9 @@ export const InputSearch = styled(InputGroup)`
 
     &:focus,
     &:hover {
-      outline: none;
+      outline: none !important;
+      border-color: none !important;
+      box-shadow: inset 0px 0px 1px 1px ${({ theme }) => theme.primary};
     }
   }
 `;
