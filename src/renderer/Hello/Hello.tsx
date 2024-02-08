273d3477cd5c31813ee9cd28b89962acf32298e3
Zahliada VM <waah777@gmail.com>
2024-02-08 15:44:52 +0200

    перестроил структуру каталогов

--- src/renderer/Hello/Hello.tsx
+++ src/renderer/Hello/Hello.tsx
@@ -1,8 +1,6 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import icon from '../../../assets/icon.svg';
 
-function Hello() {
+export function Hello() {
   return (
     <div>
       <div className="Hello">
@@ -36,15 +34,5 @@ function Hello() {
         </a>
       </div>
     </div>
-  );
-}
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
   );
 }
