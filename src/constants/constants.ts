40ec322e86acd02d769d3a3f7073f47c209f9c3d
italoviola <italoviola46@gmail.com>
2024-04-18 17:16:50 -0300

    Create logic to define actionParams with validation

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,5 @@
 export const XZ_REGEX: RegExp = /^\d{0,4}(\.\d{0,4})?$/;
+export const MCODE_REGEX: RegExp =
+  /^M([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-9]{2}|300|30[1-5])$/;
+export const FROM_1_TO_2K_REGEX: RegExp =
+  /^(2000|1[0-9]{3}|[1-9][0-9]{0,2}|[1-9])$/;
---
+++ src/integration/functions-code.ts
@@ -0,0 +1,16 @@
+import { FROM_1_TO_2K_REGEX } from 'constants/constants';
+
+export const actionParams = [
+  {
+    actionCode: 'M3',
+    aParamId: 'S',
+    aParamValue: '',
+    aParamValidation: FROM_1_TO_2K_REGEX,
+    bParamId: '',
+    bParamValue: null,
+    bParamValidation: null,
+    cParamId: '',
+    cParamValue: null,
+    cParamValidation: null,
+  },
+];
