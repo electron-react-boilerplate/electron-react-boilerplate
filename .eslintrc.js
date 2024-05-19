0f398d47a14604e28651dc8f9b31dadc7e2a2ad8
Jamie Cole <jamiejcole@icloud.com>
2024-05-19 20:15:54 +1000

    Fixed issue #3423 - updated eslint config for node resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,10 @@ module.exports = {
   settings: {
     'import/resolver': {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
-      node: {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
       webpack: {
         config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
       },
