b5adfa0a86485cd5a34ce1dbdecb7a45599173c9
DIKER0K <60111982+DIKER0K@users.noreply.github.com>
2024-08-08 01:08:38 +0500

    Add navbar icons

--- src/renderer/components/Navbar/Navbar.tsx
+++ src/renderer/components/Navbar/Navbar.tsx
@@ -1,14 +1,29 @@
 import './Navbar.css';
+import news from './Icons/News.png';
+import calls from './Icons/Calls.png';
+import schedule from './Icons/Schedule.png';
 
 function Navbar() {
   return (
     <div className="Navbar-main">
       <div className="Navbar-children">
-        <div className="Navbar-Button Navbar-Button-News">
-          <span>Новости</span>
+        <div className="Navbar-Button-Section">
+          <div className="Navbar-Button Navbar-Button-News">
+            <img src={news} alt="" className="Navbar-Icons" />
+            <span>Новости</span>
+          </div>
         </div>
-        <div className="Navbar-Button Navbar-Button-Schedule">
-          <span>Расписание</span>
+        <div className="Navbar-Button-Section">
+          <div className="Navbar-Button Navbar-Button-Schedule">
+            <img src={schedule} alt="" className="Navbar-Icons" />
+            <span>Расписание</span>
+          </div>
+        </div>
+        <div className="Navbar-Button-Section">
+          <div className="Navbar-Button Navbar-Button-Schedule">
+            <img src={calls} alt="" className="Navbar-Icons" />
+            <span>Звонки</span>
+          </div>
         </div>
       </div>
     </div>
