55a65f9b33caa2eee8e578d8d464f86c5b5903d3
DIKER0K <60111982+DIKER0K@users.noreply.github.com>
2024-08-23 17:37:23 +0500

    Add MUI

--- src/renderer/components/Navbar/Navbar.tsx
+++ src/renderer/components/Navbar/Navbar.tsx
@@ -1,34 +1,3 @@
-<<<<<<< Updated upstream
-import './Navbar.css';
-import news from './Icons/News.png';
-import calls from './Icons/Calls.png';
-import schedule from './Icons/Schedule.png';
-
-function Navbar() {
-  return (
-    <div className="Navbar-main">
-      <div className="Navbar-children">
-        <div className="Navbar-Button-Section">
-          <div className="Navbar-Button Navbar-Button-News">
-            <img src={news} alt="" className="Navbar-Icons" />
-            <span>Новости</span>
-          </div>
-        </div>
-        <div className="Navbar-Button-Section">
-          <div className="Navbar-Button Navbar-Button-Schedule">
-            <img src={schedule} alt="" className="Navbar-Icons" />
-            <span>Расписание</span>
-          </div>
-        </div>
-        <div className="Navbar-Button-Section">
-          <div className="Navbar-Button Navbar-Button-Schedule">
-            <img src={calls} alt="" className="Navbar-Icons" />
-            <span>Звонки</span>
-          </div>
-        </div>
-      </div>
-    </div>
-=======
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import BottomNavigation from '@mui/material/BottomNavigation';
@@ -82,7 +51,6 @@ function Navbar() {
         icon={<NotificationsActiveIcon fontSize="large" />}
       />
     </BottomNavigation>
->>>>>>> Stashed changes
   );
 }
 
