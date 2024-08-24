1bea90eb4357831b50863df0c845eac57d2f3c19
DIKER0K <60111982+DIKER0K@users.noreply.github.com>
2024-08-24 11:01:46 +0500

    Add Framer Motion

--- src/renderer/components/Navbar/Navbar.tsx
+++ src/renderer/components/Navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import HomeIcon from '@mui/icons-material/Home';
 import FeedIcon from '@mui/icons-material/Feed';
 import TodayIcon from '@mui/icons-material/Today';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import { motion } from 'framer-motion';
 
 function Navbar() {
   const navigate = useNavigate();
@@ -15,42 +16,51 @@ function Navbar() {
     setValue(newValue);
   };
   return (
-    <BottomNavigation
-      sx={{
-        width: 700,
-        height: 75,
-        borderRadius: '15px',
-        position: 'absolute',
-        bottom: '30px',
-        left: '50%',
-        transform: 'translate(-50%)',
-      }}
-      value={value}
-      onChange={handleChange}
+    <motion.div
+      className="animatable_nav"
+      initial={{ opacity: 0, borderRadius: '0px' }}
+      animate={{ opacity: 1, borderRadius: '16px' }}
+      transition={{ type: 'spring', stiffness: 100 }}
     >
-      <BottomNavigationAction
-        label="Главная"
-        value="Main"
-        onClick={() => navigate('/schedule')}
-        icon={<HomeIcon fontSize="large" />}
-      />
-      <BottomNavigationAction
-        label="Новости"
-        value="News"
-        icon={<FeedIcon fontSize="large" />}
-      />
-      <BottomNavigationAction
-        label="Расписание"
-        value="Schedule"
-        icon={<TodayIcon fontSize="large" />}
-      />
-      <BottomNavigationAction
-        label="Звонки"
-        value="Calls"
-        onClick={() => navigate('/schedule/calls')}
-        icon={<NotificationsActiveIcon fontSize="large" />}
-      />
-    </BottomNavigation>
+      <BottomNavigation
+        className="animatable_nav"
+        sx={{
+          width: 700,
+          height: 75,
+          borderRadius: '15px',
+          position: 'absolute',
+          zIndex: '0',
+          bottom: '30px',
+          left: '50%',
+          transform: 'translate(-50%)',
+        }}
+        value={value}
+        onChange={handleChange}
+      >
+        <BottomNavigationAction
+          label="Главная"
+          value="Main"
+          onClick={() => navigate('/schedule')}
+          icon={<HomeIcon fontSize="large" />}
+        />
+        <BottomNavigationAction
+          label="Новости"
+          value="News"
+          icon={<FeedIcon fontSize="large" />}
+        />
+        <BottomNavigationAction
+          label="Расписание"
+          value="Schedule"
+          icon={<TodayIcon fontSize="large" />}
+        />
+        <BottomNavigationAction
+          label="Звонки"
+          value="Calls"
+          onClick={() => navigate('/schedule/calls')}
+          icon={<NotificationsActiveIcon fontSize="large" />}
+        />
+      </BottomNavigation>
+    </motion.div>
   );
 }
 
--- src/renderer/pages/Calls/Calls.tsx
+++ src/renderer/pages/Calls/Calls.tsx
@@ -1,23 +1,33 @@
 import { Box } from '@mui/material';
+import { motion } from 'framer-motion';
 import calls from './calls.png';
 
 function Calls() {
   return (
-    <Box
-      sx={{
-        position: 'fixed',
-        inset: '0px',
-        margin: 'auto',
-        width: '600px',
-        height: '800px',
-      }}
+    <motion.div
+      className="animatable_calls"
+      initial={{ opacity: 0, scale: 0.5, y: 1500 }}
+      animate={{ opacity: 1, scale: 1, y: 450 }}
+      exit={{ opacity: 0, scale: 0 }}
+      transition={{ type: 'spring', stiffness: 50 }}
     >
-      <img
-        src={calls}
-        alt=" "
-        style={{ maxWidth: '100%', height: '800px', borderRadius: '16px' }}
-      />
-    </Box>
+      <Box
+        sx={{
+          position: 'fixed',
+          inset: '0px',
+          margin: 'auto',
+          width: '600px',
+          height: '800px',
+        }}
+      >
+        <img
+          src={calls}
+          alt=" "
+          className="animatable_calls"
+          style={{ maxWidth: '100%', height: '800px', borderRadius: '16px' }}
+        />
+      </Box>
+    </motion.div>
   );
 }
 
