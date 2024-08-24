655156cf51798dd674f760d10fbc5cb7cae28dd9
DIKER0K <60111982+DIKER0K@users.noreply.github.com>
2024-08-24 19:07:54 +0500

    Add slider

--- src/renderer/pages/Home/Home.tsx
+++ src/renderer/pages/Home/Home.tsx
@@ -1,11 +1,106 @@
 import { useNavigate } from 'react-router-dom';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { Box } from '@mui/material';
+import { motion } from 'framer-motion';
+import Slide1 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00001.png';
+import Slide2 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00002.png';
+import Slide3 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00003.png';
+import Slide4 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00004.png';
+import Slide5 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00005.png';
+import Slide6 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00006.png';
+import Slide7 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00007.png';
+import Slide8 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00008.png';
+import Slide9 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00009.png';
+import Slide10 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00010.png';
+import Slide11 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00011.png';
+import Slide12 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00012.png';
+import Slide13 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00013.png';
+import Slide14 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00014.png';
+import Slide15 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00015.png';
+import Slide16 from './Slides/Prezentatsia_obychnye_dni_2_semestr_00016.png';
 
 function Home() {
   const navigate = useNavigate();
   return (
-    <div className="home">
-      <title>Главная</title>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5, y: 1500 }}
+      animate={{ opacity: 1, scale: [1, 1.5, 1], y: 450 }}
+      exit={{ opacity: 0, scale: 0 }}
+      transition={{ type: 'spring', stiffness: 50 }}
+    >
+      <Box
+        sx={{
+          width: '1500px',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          borderRadius: '16px',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <Carousel
+          autoPlay
+          showArrows={false}
+          showStatus={false}
+          showIndicators={false}
+          infiniteLoop
+          showThumbs={false}
+          stopOnHover={false}
+          swipeable
+          emulateTouch
+        >
+          <div>
+            <img src={Slide1} />
+          </div>
+          <div>
+            <img src={Slide2} />
+          </div>
+          <div>
+            <img src={Slide3} />
+          </div>
+          <div>
+            <img src={Slide4} />
+          </div>
+          <div>
+            <img src={Slide5} />
+          </div>
+          <div>
+            <img src={Slide6} />
+          </div>
+          <div>
+            <img src={Slide7} />
+          </div>
+          <div>
+            <img src={Slide8} />
+          </div>
+          <div>
+            <img src={Slide9} />
+          </div>
+          <div>
+            <img src={Slide10} />
+          </div>
+          <div>
+            <img src={Slide11} />
+          </div>
+          <div>
+            <img src={Slide12} />
+          </div>
+          <div>
+            <img src={Slide13} />
+          </div>
+          <div>
+            <img src={Slide14} />
+          </div>
+          <div>
+            <img src={Slide15} />
+          </div>
+          <div>
+            <img src={Slide16} />
+          </div>
+        </Carousel>
+      </Box>
+    </motion.div>
   );
 }
 
