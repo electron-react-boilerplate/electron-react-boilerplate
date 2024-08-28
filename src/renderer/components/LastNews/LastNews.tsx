9a2d899f2dab6797c818198c75049eaf7f78ac74
DIKER0K <60111982+DIKER0K@users.noreply.github.com>
2024-08-28 21:29:57 +0500

    Update LastNews.tsx

--- src/renderer/components/LastNews/LastNews.tsx
+++ src/renderer/components/LastNews/LastNews.tsx
@@ -1,81 +1,72 @@
-import { Box } from "@mui/material";
-import axios from "axios";
-import { ReactElement, useEffect, useState } from "react";
-import { Carousel } from "react-responsive-carousel";
-import "./LastNews.css";
-import { Height } from "@mui/icons-material";
+import { Box, Paper, Typography } from '@mui/material';
+import axios from 'axios';
+import { ReactElement, useEffect, useState } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import './LastNews.css';
+import { Height } from '@mui/icons-material';
 
 const url = 'https://vk.com/skoipt_professionalitet';
 
+export default function LastNews({ fullText = false }) {
+  const [images, setImages] = useState(new Array<ReactElement>());
+  const [text, setText] = useState('Загрузка...');
 
-export default function LastNews({fullText = false})
-{
-    const [images, setImages] = useState(new Array<ReactElement>());
-    const [text, setText] = useState("загрузка...");
+  axios
+    .get(url)
+    .then((response) => {
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(response.data, 'text/html');
 
-    
-	axios.get(url)
-        .then((response) => 
-            {
-                const parser = new DOMParser();
-                const htmlDoc = parser.parseFromString(response.data, 'text/html');
+      const elements: Array<ReactElement> = [];
+      let newText = '';
 
-                let elements: Array<ReactElement> = [];
-                let newText = "";
-                
-                /*
-                * number post
-                *
-                * htmlDoc.querySelectorAll(".wall_post_text")[number]
-                */
-                let postTextEl = htmlDoc.querySelector(".wall_post_text");
-                let postContentEl = postTextEl?.parentElement;
+      /*
+       * number post
+       *
+       * htmlDoc.querySelectorAll(".wall_post_text")[number]
+       */
+      const postTextEl = htmlDoc.querySelector('.wall_post_text');
+      const postContentEl = postTextEl?.parentElement;
 
-
-                postContentEl?.querySelectorAll("div")[1]?.querySelectorAll("img").forEach(element => {
-                    if (element.classList.value.indexOf("PhotoPrimaryAttachment__imageElement") > -1 ||
-                        element.classList.value.indexOf("MediaGrid__imageElement") > -1)
-                    elements.push(
-                        <img src={element.src} />
-                    )
-                });
-
-                newText = postTextEl?.textContent ?? "";
-
-                setText(newText);
-                setImages(elements);
-        })
-        .catch((error) => {
-            setText("не удалось загрузить");
+      postContentEl
+        ?.querySelectorAll('div')[1]
+        ?.querySelectorAll('img')
+        .forEach((element) => {
+          if (
+            element.classList.value.indexOf(
+              'PhotoPrimaryAttachment__imageElement',
+            ) > -1 ||
+            element.classList.value.indexOf('MediaGrid__imageElement') > -1
+          )
+            elements.push(<img src={element.src} />);
         });
 
-    return (
-        <Box
-            sx={{
-                maxHeight: '300px',
-                position: 'relative',
-                top: '20px',
-                left: '50%',
-                borderRadius: '16px',
-                transform: 'translate(-50%, 0)',
-            }}
-        >
-            <Carousel
-                autoPlay
-                showArrows={false}
-                showStatus={false}
-                showIndicators={false}
-                infiniteLoop
-                showThumbs={false}
-                stopOnHover={false}
-                swipeable
-                emulateTouch
-            >
-                {images}
-            </Carousel>
-            <p className="LastNews__text">
-                {text}
-            </p>
+      newText = postTextEl?.textContent ?? '';
+
+      setText(newText);
+      setImages(elements);
+    })
+    .catch((error) => {
+      setText('не удалось загрузить');
+    });
+
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        position: 'fixed',
+        inset: '0px',
+        margin: 'auto',
+        width: '1000px',
+        height: '800px',
+      }}
+    >
+      <Box sx={{ margin: '15px' }}>
+        <Typography variant="h6">{text}</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center', height: '300px' }}>
+          {images}
         </Box>
-    )
+      </Box>
+    </Paper>
+  );
 }
