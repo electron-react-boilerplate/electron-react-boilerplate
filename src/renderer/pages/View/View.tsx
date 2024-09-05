173f6dbdd36a5eb195b3e37ea164fd64a71e65f8
okube <okube@mail.ru>
2024-09-05 16:03:26 +0500

    update Schedule.tsx and View.tsx

--- src/renderer/pages/View/View.tsx
+++ src/renderer/pages/View/View.tsx
@@ -1,11 +1,26 @@
+import axios from "axios";
+import React from "react";
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
 
 
 
 export default function View()
 {
-    return (
-      <div>
-        {window.location.href}
-      </div>
-    );
+  const [schedule, setSchedule] = React.useState( <div>Загрузка...</div> );
+  const [searchParams] = useSearchParams();
+
+  axios.get("https://skoipt.ru/images/rs/Schedule_htm/"+searchParams.get("group"))
+    .then((response) => {
+      setSchedule(response.data)
+    })
+    .catch((error) => {
+      console.log("load failed" + error)
+    });
+
+  return (
+    <div>
+      {searchParams.get("group")}
+      {schedule}
+    </div>
+  );
 }
