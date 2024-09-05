230696c55434c8abaab2f1cb74f32b7e91440a14
Amila Welihinda <amilajack@gmail.com>
2024-09-05 12:52:52 -0700

    refactor: cleanup link-modules conditions

--- .erb/scripts/link-modules.ts
+++ .erb/scripts/link-modules.ts
@@ -4,10 +4,11 @@ import webpackPaths from '../configs/webpack.paths';
 const { srcNodeModulesPath, appNodeModulesPath, erbNodeModulesPath } =
   webpackPaths;
 
-if (!fs.existsSync(srcNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
-  fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
-}
-
-if (!fs.existsSync(erbNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
-  fs.symlinkSync(appNodeModulesPath, erbNodeModulesPath, 'junction');
+if (fs.existsSync(appNodeModulesPath)) {
+  if (!fs.existsSync(srcNodeModulesPath)) {
+    fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
+  }
+  if (!fs.existsSync(erbNodeModulesPath)) {
+    fs.symlinkSync(appNodeModulesPath, erbNodeModulesPath, 'junction');
+  }
 }
