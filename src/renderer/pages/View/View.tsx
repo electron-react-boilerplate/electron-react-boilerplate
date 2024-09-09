451dd7ed646680fa8441770b649214fb66e64692
reMud <killersan0010@gmail.com>
2024-09-09 14:56:06 +0500

    feat(View.tsx): add schdeule parser

--- src/renderer/pages/View/View.tsx
+++ src/renderer/pages/View/View.tsx
@@ -1,78 +1,143 @@
-import { CircularProgress, Paper } from '@mui/material';
-import axios from 'axios';
-import React from 'react';
-import { useSearchParams } from 'react-router-dom';
-import parse from 'html-react-parser';
+import { Alert, CircularProgress, Paper } from "@mui/material";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
 
-export default function View() {
-  const [schedules, setSchedules] = React.useState<JSX.Element[]>([
-    <CircularProgress sx={{ marginTop: '280px' }} />,
-  ]);
+function getNextMondayTimestamp() {
+  const now = new Date();
+  const currentDay = now.getDay();
+  const daysUntilMonday = (currentDay === 0 ? 0 : 7 - currentDay);
+  const nextMonday = new Date(now.getTime() + daysUntilMonday * 24 * 60 * 60 * 1000);
+  nextMonday.setHours(0, 0, 0, 0);
+  return nextMonday.getTime();
+}
+
+function Parse(group: string, handleChange: Function)
+{
+  let url = "https://skoipt.ru/images/rs/Schedule_htm/" + group + ".htm";
+
+  console.log("request: " + url)
+
+  axios.get(url, { responseType: 'arraybuffer' })
+  .then((response) => {
+    /*
+     * schedule - matrix
+     * [i] - rows
+     * [j] - cells
+     * 
+     * [i][j] -> [
+     *  text, width, height, colspan, rowspan
+     * ]
+    */
+    let schedule: any = []
+    const decoder = new TextDecoder('windows-1251');
+    const decodedData = decoder.decode(new Uint8Array(response.data));
+    const htmlDoc = new DOMParser().parseFromString(decodedData, 'text/html');
+    console.log(htmlDoc)
+    let table = htmlDoc.querySelectorAll(".MsoNormalTable")[1];
+    
+    let rows = table.querySelectorAll("tr");
+    for (let i = 0; i < rows.length; i++)
+    {
+      let cells = rows[i].querySelectorAll("td");
+      schedule[i] = [];
+      for (let j = 0; j < cells.length; j++)
+      {
+        let style: any = [];
+
+        style.push(cells[j].outerText);
+        style.push(cells[j].style.width);
+        style.push(cells[j].style.height);
+        style.push(cells[j].colSpan);
+        style.push(cells[j].rowSpan);
+
+        schedule[i][j] = style;
+      }
+    }
+    
+    handleChange(schedule)
+
+    let schedules = JSON.parse(localStorage.getItem("schedules") ?? "{}");
+
+    schedules[group] = {}
+    schedules[group].expires = getNextMondayTimestamp();
+    schedules[group].schedule = schedule
+
+    localStorage.setItem("schedules", JSON.stringify(schedules))
+  })
+  .catch((error) => {
+    console.log("load failed: " + error)
+  });
+}
+
+function Load(group: string, handleChange: Function)
+{
+  let schedules = JSON.parse(localStorage.getItem("schedules") ?? "0");
+  
+  if (schedules == null || 
+    schedules[group] == null ||
+    schedules[group].expires < Date.now())
+  {
+    Parse(group, handleChange)
+  }
+  else
+  {
+    handleChange(schedules[group].schedule)
+  }
+}
+
+export default function View()
+{
+  const [schedule, setSchedule] = React.useState( <CircularProgress sx={{marginTop: "280px"}}/> );
   const [searchParams] = useSearchParams();
-  const url = `https://skoipt.ru/images/rs/Schedule_htm/${searchParams.get('group')}.htm`;
 
-  console.log(`request: ${url}`);
+  function changeSchedule(newSchedule: any[])
+  {
+    console.log(newSchedule)
+    let rows: JSX.Element[] = []
 
-  React.useEffect(() => {
-    axios
-      .get(url, { responseType: 'arraybuffer' })
-      .then((response) => {
-        // Create a TextDecoder instance for windows-1251
-        const decoder = new TextDecoder('windows-1251');
-        // Decode the array buffer
-        const decodedData = decoder.decode(new Uint8Array(response.data));
+    newSchedule.forEach((trs: any[]) => 
+    {
+      let tr: JSX.Element[] = []
 
-        const htmlDoc = new DOMParser().parseFromString(
-          decodedData,
-          'text/html',
-        );
-
-        // Find all tables with the class 'MsoNormalTable'
-        const tables = Array.from(htmlDoc.querySelectorAll('.MsoNormalTable'));
-
-        // Convert all tables to JSX elements
-        const tableElements = tables.map((table) =>
-          parse(table.outerHTML)
-        );
-
-        setSchedules(tableElements);
-      })
-      .catch((error) => {
-        console.log(`load failed ${error}`);
+      trs.forEach((td: any[]) => 
+      {
+        tr.push(
+          <td 
+            colSpan={td[3]} 
+            rowSpan={td[4]}
+            style={{
+              width: td[1],
+              height: td[2],
+            }}
+          >
+            {td[0]}
+          </td>
+        )
       });
-  }, [url]);
 
+      rows.push(<tr>{tr}</tr>)
+    });
+
+    setSchedule(<tbody>{rows}</tbody>);
+  }
+
+  useEffect(() => {
+    Load(searchParams.get("group") ?? "", changeSchedule) 
+  }, [])
+  
   return (
-    <Paper
+    <Paper 
       sx={{
-        position: 'fixed',
-        inset: '0px',
+        width: '1030px',
+        minHeight: '600px',
+        height: 'fit-content',
+        textAlign: 'center',
         margin: 'auto',
-        width: '1200px',  // Установите фиксированную ширину
-        height: '800px', // Установите фиксированную высоту
-        overflow: 'auto', // Добавьте прокрутку, если содержимое превышает размеры
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        boxSizing: 'border-box', // Убедитесь, что padding и border включены в размеры
+        marginTop: '140px'
       }}
     >
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          overflow: 'hidden',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-          transform: 'scale(0.8)', // Масштабируйте содержимое
-          transformOrigin: 'top left', // Масштабируйте от верхнего левого угла
-        }}
-      >
-        {schedules}
-      </div>
+      {schedule}
     </Paper>
   );
 }
