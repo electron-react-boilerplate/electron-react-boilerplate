fe3e1cbf2ffe2e71d871683d934c20779504dfb4
DIKER0K <60111982+DIKER0K@users.noreply.github.com>
2024-09-09 23:01:17 +0500

    Edit News. Add schedule table animation

--- src/renderer/pages/News/News.tsx
+++ src/renderer/pages/News/News.tsx
@@ -1,13 +1,121 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Box, Paper, Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from 'react-compare-slider';
+import { Carousel } from 'react-responsive-carousel';
 import useInactivityRedirect from '../../components/Scripts/useInactivityRedirect';
+import before1 from './Temp/before_1.png';
+import after1 from './Temp/after_1.png';
+import before2 from './Temp/before_2.png';
+import after2 from './Temp/after_2.png';
+import after_new_1 from './Temp/after_new_1.png';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import LastNews from '../../components/LastNews/LastNews';
 
 function News() {
+  const [sliderPosition, setSliderPosition] = useState(50);
+  const containerRef = useRef(null);
+
+  const handleSliderMove = (e) => {
+    const containerRect = containerRef.current.getBoundingClientRect();
+    const newPosition =
+      ((e.clientX - containerRect.left) / containerRect.width) * 100;
+    setSliderPosition(Math.max(0, Math.min(100, newPosition)));
+  };
   useInactivityRedirect();
   return (
-    <div>
-      <LastNews />
-    </div>
+    // <div>
+    //   <LastNews />
+    // </div>
+    <Box
+      sx={{
+        position: 'fixed',
+        inset: '0px',
+        margin: 'auto',
+        width: '800px',
+        height: '600px',
+      }}
+    >
+      <motion.div
+        initial={{ opacity: 0, scale: 0, y: 500 }}
+        animate={{ opacity: 1, scale: 1, y: 0 }}
+        exit={{ opacity: 0, scale: 0 }}
+        transition={{ type: 'spring', stiffness: 50 }}
+      >
+        <Paper
+          elevation={5}
+          sx={{
+            width: '800px',
+            height: '600px',
+            display: 'flex',
+            justifyContent: 'center',
+            textAlign: 'center',
+            flexWrap: 'wrap',
+          }}
+        >
+          <Box
+            sx={{
+              marginLeft: '15px',
+              marginRight: '15px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography variant="h6" sx={{ marginBottom: '15px' }}>
+              Мы обновились!
+            </Typography>
+            <Carousel
+              autoPlay
+              showArrows
+              showStatus={false}
+              showIndicators={false}
+              infiniteLoop
+              showThumbs={false}
+              stopOnHover
+              swipeable
+              emulateTouch
+            >
+              <div>
+                <ReactCompareSlider
+                  itemOne={
+                    <ReactCompareSliderImage src={before1} alt="Image one" />
+                  }
+                  itemTwo={
+                    <ReactCompareSliderImage src={after1} alt="Image two" />
+                  }
+                />
+                <Typography variant="body1" sx={{ marginBottom: '15px' }}>
+                  Новый интерфейс
+                </Typography>
+              </div>
+              <div>
+                <img src={after_new_1} alt="" />
+                <Typography variant="body1" sx={{ marginBottom: '15px' }}>
+                  Новая кнопка "Новости"
+                </Typography>
+              </div>
+              <div>
+                <ReactCompareSlider
+                  itemOne={
+                    <ReactCompareSliderImage src={before2} alt="Image one" />
+                  }
+                  itemTwo={
+                    <ReactCompareSliderImage src={after2} alt="Image two" />
+                  }
+                />
+                <Typography variant="body1" sx={{ marginBottom: '15px' }}>
+                  Новый интерфейс выбора группы
+                </Typography>
+              </div>
+            </Carousel>
+          </Box>
+        </Paper>
+      </motion.div>
+    </Box>
   );
 }
 
--- src/renderer/pages/View/View.tsx
+++ src/renderer/pages/View/View.tsx
@@ -1,141 +1,140 @@
-import { Alert, Button, ButtonBase, CircularProgress, Paper } from "@mui/material";
-import axios from "axios";
-import React, { useEffect } from "react";
-import { useLocation, useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { Button, CircularProgress, Paper } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import "./View.css"
+import './View.css';
 
 function getNextMondayTimestamp() {
   const now = new Date();
   const currentDay = now.getDay();
-  const daysUntilMonday = (currentDay === 0 ? 0 : 7 - currentDay);
-  const nextMonday = new Date(now.getTime() + daysUntilMonday * 24 * 60 * 60 * 1000);
+  const daysUntilMonday = currentDay === 0 ? 0 : 7 - currentDay;
+  const nextMonday = new Date(
+    now.getTime() + daysUntilMonday * 24 * 60 * 60 * 1000,
+  );
   nextMonday.setHours(0, 0, 0, 0);
   return nextMonday.getTime();
 }
 
-function Parse(group: string, handleChange: Function)
-{
-  let url = "https://skoipt.ru/images/rs/Schedule_htm/" + group + ".htm";
+function Parse(group: string, handleChange: Function) {
+  const url = `https://skoipt.ru/images/rs/Schedule_htm/${group}.htm`;
 
-  console.log("request: " + url)
+  console.log(`request: ${url}`);
 
-  axios.get(url, { responseType: 'arraybuffer' })
-  .then((response) => {
-    /*
-     * schedule - matrix
-     * [i] - rows
-     * [j] - cells
-     * 
-     * [i][j] -> [
-     *  text, width, height, colspan, rowspan
-     * ]
-    */
-    let schedule: any = []
-    const decoder = new TextDecoder('windows-1251');
-    const decodedData = decoder.decode(new Uint8Array(response.data));
-    const htmlDoc = new DOMParser().parseFromString(decodedData, 'text/html');
-    console.log(htmlDoc)
-    let table = htmlDoc.querySelectorAll(".MsoNormalTable")[1];
-    
-    let rows = table.querySelectorAll("tr");
-    for (let i = 0; i < rows.length; i++)
-    {
-      let cells = rows[i].querySelectorAll("td");
-      schedule[i] = [];
-      for (let j = 0; j < cells.length; j++)
-      {
-        let style: any = [];
+  axios
+    .get(url, { responseType: 'arraybuffer' })
+    .then((response) => {
+      /*
+       * schedule - matrix
+       * [i] - rows
+       * [j] - cells
+       *
+       * [i][j] -> [
+       *  text, width, height, colspan, rowspan
+       * ]
+       */
+      const schedule: any = [];
+      const decoder = new TextDecoder('windows-1251');
+      const decodedData = decoder.decode(new Uint8Array(response.data));
+      const htmlDoc = new DOMParser().parseFromString(decodedData, 'text/html');
+      console.log(htmlDoc);
+      const table = htmlDoc.querySelectorAll('.MsoNormalTable')[1];
 
-        style.push(cells[j].outerText);
-        style.push(cells[j].style.width);
-        style.push(cells[j].style.height);
-        style.push(cells[j].colSpan);
-        style.push(cells[j].rowSpan);
+      const rows = table.querySelectorAll('tr');
+      for (let i = 0; i < rows.length; i++) {
+        const cells = rows[i].querySelectorAll('td');
+        schedule[i] = [];
+        for (let j = 0; j < cells.length; j++) {
+          const style: any = [];
 
-        schedule[i][j] = style;
+          style.push(cells[j].outerText);
+          style.push(cells[j].style.width);
+          style.push(cells[j].style.height);
+          style.push(cells[j].colSpan);
+          style.push(cells[j].rowSpan);
+
+          schedule[i][j] = style;
+        }
       }
-    }
-    
-    handleChange(schedule)
 
-    let schedules = JSON.parse(localStorage.getItem("schedules") ?? "{}");
+      handleChange(schedule);
 
-    schedules[group] = {}
-    schedules[group].expires = getNextMondayTimestamp();
-    schedules[group].schedule = schedule
+      const schedules = JSON.parse(localStorage.getItem('schedules') ?? '{}');
 
-    localStorage.setItem("schedules", JSON.stringify(schedules))
-  })
-  .catch((error) => {
-    console.log("load failed: " + error)
-  });
+      schedules[group] = {};
+      schedules[group].expires = getNextMondayTimestamp();
+      schedules[group].schedule = schedule;
+
+      localStorage.setItem('schedules', JSON.stringify(schedules));
+    })
+    .catch((error) => {
+      console.log(`load failed: ${error}`);
+    });
 }
 
-function Load(group: string, handleChange: Function)
-{
-  let schedules = JSON.parse(localStorage.getItem("schedules") ?? "0");
-  
-  if (schedules == null || 
+function Load(group: string, handleChange: Function) {
+  const schedules = JSON.parse(localStorage.getItem('schedules') ?? '0');
+
+  if (
+    schedules == null ||
     schedules[group] == null ||
-    schedules[group].expires < Date.now())
-  {
-    Parse(group, handleChange)
-  }
-  else
-  {
-    handleChange(schedules[group].schedule)
+    schedules[group].expires < Date.now()
+  ) {
+    Parse(group, handleChange);
+  } else {
+    handleChange(schedules[group].schedule);
   }
 }
 
-export default function View()
-{
-  const [schedule, setSchedule] = React.useState( <CircularProgress sx={{marginTop: "280px"}}/> );
+export default function View() {
+  const [schedule, setSchedule] = React.useState(
+    <CircularProgress sx={{ marginTop: '280px' }} />
+  );
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
-  function changeSchedule(newSchedule: any[])
-  {
-    console.log(newSchedule)
-    let rows: JSX.Element[] = []
+  function changeSchedule(newSchedule) {
+    const rows = [];
 
-    newSchedule.forEach((trs: any[]) => 
-    {
-      let tr: JSX.Element[] = []
+    newSchedule.forEach((trs, i) => {
+      const tr = [];
 
-      trs.forEach((td: any[]) => 
-      {
+      trs.forEach((td, j) => {
         tr.push(
-          <td 
-            colSpan={td[3]} 
+          <motion.td
+            key={j}
+            colSpan={td[3]}
             rowSpan={td[4]}
             style={{
               width: td[1],
               height: td[2],
             }}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: i * 0.1 + j * 0.05 }}
           >
             {td[0]}
-          </td>
-        )
+          </motion.td>
+        );
       });
 
-      rows.push(<tr>{tr}</tr>)
+      rows.push(
+        <motion.tr key={i}>
+          {tr}
+        </motion.tr>
+      );
     });
 
-    setSchedule(
-      <tbody className="View__table">
-        {rows}
-      </tbody>
-    );
+    setSchedule(<tbody className="View__table">{rows}</tbody>);
   }
 
   useEffect(() => {
-    Load(searchParams.get("group") ?? "", changeSchedule) 
-  }, [])
-  
+    Load(searchParams.get('group') ?? '', changeSchedule);
+  }, []);
+
   return (
     <div>
-      <Paper 
+      <Paper
         sx={{
           minWidth: '1030px',
           minHeight: '200px',
@@ -144,27 +143,29 @@ export default function View()
           position: 'absolute',
           top: '50%',
           left: '50%',
-          transform: 'translate(-50%, -50%) scale(1.5)'
+          padding: '15px',
+          transform: 'translate(-50%, -50%) scale(1.5)',
         }}
       >
         {schedule}
       </Paper>
       <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.1 }}
-        >
-        <Button 
+        initial={{ opacity: '0' }}
+        animate={{ opacity: '1' }}
+        transition={{ type: 'spring', stiffness: 50, delay: '0.1' }}
+      >
+        <Button
           onClick={() => navigate('/schedule')}
           variant="contained"
-          sx = {{
+          sx={{
             position: 'absolute',
             width: 240,
             height: 75,
             borderRadius: '15px',
-            left: "calc(50% - 700px)",
+            left: 'calc(50% - 700px)',
             bottom: '30px',
-          }}>
+          }}
+        >
           Назад
         </Button>
       </motion.div>
