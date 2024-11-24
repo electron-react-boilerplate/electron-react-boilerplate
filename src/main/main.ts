4a9358e59b15238a380e73fdb78baabf540aff57
RohanNethala <rohroh343@gmail.com>
2024-11-23 19:09:57 -0500

    ...

--- src/main/main.ts
+++ src/main/main.ts
@@ -16,11 +16,6 @@ import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
 
 
-ipcMain.on('ipc-example', async (event, arg) => {
-  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-  console.log(msgTemplate(arg));
-  event.reply('ipc-example', msgTemplate('pong'));
-});
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
