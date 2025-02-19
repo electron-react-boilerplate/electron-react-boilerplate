ea4a59924f6a39025573eeb45acac83a75105aa2
italoviola <italoviola46@gmail.com>
2025-02-19 20:47:41 -0300

    Minor adjust

--- src/components/ContourForm/index.tsx
+++ src/components/ContourForm/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { ContourType, Machining } from 'types/part';
 
 import FormField from 'components/FormField';
