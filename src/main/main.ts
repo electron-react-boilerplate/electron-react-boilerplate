437a96dc87b24c1bf917378da6650d0ac05b7b99
Amila Welihinda <amilajack@gmail.com>
2025-03-17 14:16:24 -0700

    fix: ts import register and debug devtools

--- src/main/main.ts
+++ src/main/main.ts
@@ -40,7 +40,7 @@ const isDebug =
   process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
 if (isDebug) {
-  require('electron-debug')();
+  require('electron-debug').default();
 }
 
 const installExtensions = async () => {
