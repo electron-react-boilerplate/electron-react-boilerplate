f6e6ebfc20a0f933fa443db09f9e1ac1a3798936
Kevin Hu <kevin.hu.w@gmail.com>
2025-10-16 07:06:24 -0400

    Add check for debugging args

--- src/main/main.ts
+++ src/main/main.ts
@@ -41,6 +41,15 @@ const isDebug =
 
 if (isDebug) {
   require('electron-debug').default();
+  let port = '9223';
+  if (process.env.MAIN_ARGS) {
+    port = (
+      [...process.env.MAIN_ARGS.matchAll(/"[^"]+"|[^\s"]+/g)]
+        .flat()
+        .filter((str) => str.includes('debugging-port'))[0] || '=9223'
+    ).split('=')[1];
+  }
+  app.commandLine.appendSwitch('remote-debugging-port', port);
 }
 
 const installExtensions = async () => {
